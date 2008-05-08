be57d425110233af9158a7ca3bdb39e162ac2f81
kovarex <kovarex@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2008-05-08 11:20:13 +0000

    Documentation front page (index.h in BWAPI root) configuration-step.bat - generate doxygen configuration, shows error log, opens documentation
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@77 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/BWAPI/index.h
@@ -0,0 +1,34 @@
+/** @mainpage Welcome to Broodwar api documentaiton
+
+@section intro Introduction
+
+Broodwar API is library allowing simple access to broodwar relevant data and 
+unit control.
+The library is not inteded to make hacks or bw tools, but to make broodwar ai 
+project possible, we are still considering how to avoid using this library to 
+use some hack tools.
+
+@section tutorial Tutorial
+
+@section install Installation
+
+The library target is Windows Xp + Visual studio 2005 only for now. 
+<ul>
+  <li>1) Download latest sources. </li>
+  <li>2) Get Chaos launcher </li>
+  <li>3) Set Environment "ChaosDir" variable in windows (ControllPanels->System->Advanced->Envirenment variables) to your chaos launcher directory. </li> 
+  <li>4) Compile </li>
+  <li>5) Run Chaos launcher and click to use the BWAPI addon </li>
+</ul>
+
+@section parametrization Parametrization of BW API
+
+@section design Overall Design
+
+@todo Ingame offset
+@todo lot of more
+*/
+
+/** @page webpages Related webpages
+http://code.google.com/p/bwapi
+*/
