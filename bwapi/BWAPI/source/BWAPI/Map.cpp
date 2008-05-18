751c328a77c9ccc5abc55bddcb8aa3d448a23fa5
kovarex <kovarex@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2008-05-18 11:39:58 +0000

    The map tileType information reading works now.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@152 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/source/BWAPI/Map.cpp
+++ bwapi/BWAPI/source/BWAPI/Map.cpp
@@ -1,10 +1,12 @@
 #include "Map.h"
+#include "..//BW//TileSet.h"
+#include "..//BW//TileType.h"
 namespace BWAPI
 {
   //------------------------------- GET TILE --------------------------------
   BW::TileID Map::getTile(int x, int y)
   {
-    return *(*BW::BWXFN_MapTileArray + (x + y*Map::getWidth())) & 0x2FF;
+    return *(*BW::BWXFN_MapTileArray + x + y*Map::getWidth());
   }
   //------------------------------ GET WIDTH --------------------------------
   u16 Map::getWidth()
@@ -16,5 +18,35 @@ namespace BWAPI
   {
     return *BW::BWXFN_MapSizeY;
   }
-  //-------------------------------------------------------------------------
+  //------------------------------- SAVE BUILDABILITY MAP -------------------
+  void Map::saveBuildabilityMap(const std::string& fileName)
+  {
+    FILE* f = fopen(fileName.c_str(),"wt");
+    fprintf(f, "Buildability map for currently opened map\n");
+    fprintf(f, "Map file: %s\n", Map::getFileName());
+    fprintf(f, "Map width: %d\n", BWAPI::Map::getWidth());
+    fprintf(f, "Map height: %d\n", BWAPI::Map::getHeight());
+    fprintf(f, "X = not buildable\n");
+    fprintf(f, ". = buildable\n");
+                
+
+    for (int y = 0; y < BWAPI::Map::getHeight(); y++)
+    {
+      for (int x = 0; x < BWAPI::Map::getWidth(); x++)
+      {
+        if ((((BW::TileSet::getTileType(BWAPI::Map::getTile(x, y))->buildability >> 4) & 0X8)) == 0)
+          fprintf(f, ".");
+        else
+          fprintf(f, "X");
+      }
+      fprintf(f, "\n");
+    }
+    fclose(f);             
+  }
+  //----------------------------------- GET FILE NAME -------------------------
+  char* Map::getFileName(void)
+  {
+    return BW::BWXFN_CurrentMapFileName;
+  }
+  //---------------------------------------------------------------------------
 }
--- bwapi/BWAPI/source/BWAPI/Map.h
+++ bwapi/BWAPI/source/BWAPI/Map.h
@@ -1,10 +1,13 @@
 #include "..//BW//Offsets.h"
+#include <string>
 namespace BWAPI
 {
   /**
    * Interface to acces broodwar map data.
    * @Todo make constructor/destructor for the class
-   * and load the map into some reasonable format.
+   * and load the map into some reasonable format or at least
+   * make pointes to the beginning of lines, so the getTile
+   * function doesn't have to use the slow multiplication.
    */
   class Map
   {
@@ -12,5 +15,9 @@ namespace BWAPI
      static BW::TileID getTile(int x, int y);
      static u16 getWidth();
      static u16 getHeight();
+     /** Gets file name of the currently opened map by broodwar */
+     static char* getFileName();
+     /** Saves the buildability map for the currently opened map to the specified file. */
+     static void saveBuildabilityMap(const std::string& fileName);
   };
 }
