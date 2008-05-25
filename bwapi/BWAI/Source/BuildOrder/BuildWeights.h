ebdc9d09a43bc0ec8c2e7317addfa4463a62b18d
kovarex <kovarex@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2008-05-25 19:23:27 +0000

    It works to build expansion - preparing to do automated building of units according to build order definition.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@229 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/BWAI/Source/BuildOrder/BuildWeights.h
@@ -0,0 +1,9 @@
+namespace BuildOrder
+{
+  class BuildWeights
+  {
+    UnitType factory;
+    std::list<std::pair<UnitType, int> > weights;
+    
+  };
+}
--- bwapi/BWAI/Source/BuildOrder/Root.cpp
+++ bwapi/BWAI/Source/BuildOrder/Root.cpp
@@ -33,6 +33,8 @@ namespace BuildOrder
     for (std::list<Branch*>::iterator i = this->buildOrders.begin(); i != this->buildOrders.end(); ++i)
       delete *i;
     this->buildOrders.clear();
+    for (std::map<std::string, std::pair<BW::UnitType,int>* >::iterator i = this->weights.begin(); i != this->weights.end(); ++i)
+      delete (*i).second;
   }
   //---------------------------------------------------------------------------
 }
