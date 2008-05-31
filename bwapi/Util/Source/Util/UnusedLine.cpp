bfb0712294f4099b176d997926990eac9548b1e6
kovarex <kovarex@gmail.com>
2008-05-31 12:25:14 +0000

    git-svn-id: https://bwapi.googlecode.com/svn/trunk@268 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/Util/Source/Util/UnusedLine.cpp
+++ bwapi/Util/Source/Util/UnusedLine.cpp
@@ -1,12 +1,15 @@
 #include "Unusedline.h"
-//------------------------------------ CONSTRUCTOR -----------------------------
-UnusedLine::UnusedLine(const std::string &line)
- {
-  this->line = line;
- }
-//-------------------------------------- GET LINE ------------------------------
-std::string UnusedLine::getLine(void)
- {
-  return line;
- }
-//------------------------------------------------------------------------------
+namespace Util
+{
+  //------------------------------------ CONSTRUCTOR -----------------------------
+  UnusedLine::UnusedLine(const std::string &line)
+   {
+    this->line = line;
+   }
+  //-------------------------------------- GET LINE ------------------------------
+  std::string UnusedLine::getLine(void)
+   {
+    return line;
+   }
+  //------------------------------------------------------------------------------
+}
