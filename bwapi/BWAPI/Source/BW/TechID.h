e8f0a9227e4098c44fb5a1b02f8deb839fd5a23b
Adam Heinermann <aheinerm@gmail.com>
2008-08-14 12:52:25 +0000

    git-svn-id: https://bwapi.googlecode.com/svn/trunk@411 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/TechID.h
+++ bwapi/BWAPI/Source/BW/TechID.h
@@ -5,7 +5,7 @@
 namespace BW
 {
   /**
-   * Direct mapping of bw tech identification. 
+   * Direct mapping of bw tech identification.
    * Source is http://www.staredit.net/wiki/Techdata.dat_Entry_Listing
    */
   namespace TechID
@@ -16,46 +16,46 @@ namespace BW
       Lockdown          = 0x01,
       EMPShockwave      = 0x02,
       SpiderMines       = 0x03,
-      ScannerSweep      = 0x04,
+      ScannerSweep      = 0x04, // (default)
       TankSiegeMode     = 0x05,
-      DefensiveMatrix   = 0x06,
+      DefensiveMatrix   = 0x06, // (default)
       Irradiate         = 0x07,
       YamatoGun         = 0x08,
       CloakingField     = 0x09,
       PersonnelCloaking = 0x0A,
       Burrowing         = 0x0B,
-      Infestation       = 0x0C,
+      Infestation       = 0x0C, // (default)
       SpawnBroodlings   = 0x0D,
-      DarkSwarm         = 0x0E,
+      DarkSwarm         = 0x0E, // (default)
       Plague            = 0x0F,
       Consume           = 0x10,
       Ensnare           = 0x11,
-      Parasite          = 0x12,
+      Parasite          = 0x12, // (default)
       PsionicStorm      = 0x13,
       Hallucination     = 0x14,
       Recall            = 0x15,
       StasisField       = 0x16,
-      ArchonWarp        = 0x17,
+      ArchonWarp        = 0x17, // (default)
       Restoration       = 0x18,
       DisruptionWeb     = 0x19,
-      UnusedTech26      = 0x1A,
+      UnusedTech26      = 0x1A, // (unused)
       MindControl       = 0x1B,
-      DarkArchonMeld    = 0x1C,
-      Feedback          = 0x1D,
+      DarkArchonMeld    = 0x1C, // (default)
+      Feedback          = 0x1D, // (default)
       OpticalFlare      = 0x1E,
       Maelstorm         = 0x1F,
       LurkerAspect      = 0x20,
-      UnusedTech33      = 0x21,
-      Healing           = 0x22,
-      UnusedTech35      = 0x23,
-      UnusedTech36      = 0x24,
-      UnusedTech37      = 0x25,
-      UnusedTech38      = 0x26,
-      UnusedTech39      = 0x27,
-      UnusedTech40      = 0x28,
-      UnusedTech41      = 0x29,
-      UnusedTech42      = 0x2A,
-      UnusedTech43      = 0x2B,
+      UnusedTech33      = 0x21, // (default; possibly liftoff)
+      Healing           = 0x22, // (default)
+      UnusedTech35      = 0x23, // (unused)
+      UnusedTech36      = 0x24, // (unused)
+      UnusedTech37      = 0x25, // (unused)
+      UnusedTech38      = 0x26, // (unused)
+      UnusedTech39      = 0x27, // (unused)
+      UnusedTech40      = 0x28, // (unused)
+      UnusedTech41      = 0x29, // (unused)
+      UnusedTech42      = 0x2A, // (unused)
+      UnusedTech43      = 0x2B, // (unused)
       None              = 0x2C
     };
   }
