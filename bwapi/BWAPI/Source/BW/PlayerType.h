9d15260eede44565f4d66ddf3b8a63b0f01bb51e
Adam Heinermann <aheinerm@gmail.com>
2008-08-14 12:45:30 +0000

    git-svn-id: https://bwapi.googlecode.com/svn/trunk@409 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/PlayerType.h
+++ bwapi/BWAPI/Source/BW/PlayerType.h
@@ -9,7 +9,12 @@ namespace BW
       Computer         = 1,
       Human            = 2,
       Rescuable        = 3,
+      Unknown0         = 4,
+      ComputerSlot     = 5,
+      OpenSlot         = 6,
       Neutral          = 7,
+      ClosedSlot       = 8,
+      Unknown1         = 9,
       HumanDefeated    = 10, /**< Left */
       ComputerDefeated = 11  /**< Left */
     };
