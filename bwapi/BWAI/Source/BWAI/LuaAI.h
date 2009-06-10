3fef8eac29b5de40a8d596f30769884c3e6f81a7
Adam Heinermann <aheinerm@gmail.com>
2009-06-10 00:54:09 +0000

    Ok. I'm sure that THIS build works. I tested it. Making a special note because I don't remember if I tested the last commit.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@602 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAI/Source/BWAI/LuaAI.h
+++ bwapi/BWAI/Source/BWAI/LuaAI.h
@@ -20,6 +20,30 @@ namespace BWAI
       lua_Debug luaDebug;
     // interfaces to BWAI and BWAPI go here
       static int sendText(lua_State *l);
+/*      static int getMapSize(lua_State *l);
+      static int getMapName(lua_State *l);
+      static int getTerrainInfo(lua_State *l);
+      static int getStartLocation(lua_State *l);
+      static int getCurrrentPlayer(lua_State *l);
+      static int getMapHash(lua_State *l);
+      static int getPlayerName(lua_State *l);
+      static int getPlayerRace(lua_State *l);
+      static int getMinerals(lua_State *l);
+      static int getGas(lua_State *l);
+      static int getSupplyTotal(lua_State *l);
+      static int getSupplyUsed(lua_State *l);
+      static int getAllUnitCount(lua_State *l);
+      static int getCompletedUnitCount(lua_State *l);
+      static int getIncompletedUnitCount(lua_State *l);
+      static int getDeaths(lua_State *l);
+      static int getKills(lua_State *l);
+      static int getForceName(lua_State *l);
+      static int getPlayerOwner(lua_State *l);
+      static int getUnitCountOnTile(lua_State *l);
+      static int isFlagEnabled(lua_State *l);
+      static int enableFlag(lua_State *l);
+*/
+
 
   };  // class LUA
 } // namespace BWAI
