dc06f611fbfda5975f680248b18efc6ff4ef9370
Adam Heinermann <aheinerm@gmail.com>
2009-06-12 03:21:47 +0000

    LUA::getCurrentPlayer LUA::getMapHash LUA::getRace LUA::getForceName LUA::isUpgrading LUA::getUpgrade LUA::isResearching LUA::isResearched LUA::getKills LUA::getDeaths LUA::getAllUnits LUA::getCompleteUnits LUA::getIncompleteUnits
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@614 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAI/Source/BWAI/LuaAI.cpp
+++ bwapi/BWAI/Source/BWAI/LuaAI.cpp
@@ -17,7 +17,17 @@ namespace BWAI
     lua_register(lua, "currentPlayer", getCurrentPlayer);
     lua_register(lua, "playerName", getPlayerName);
     lua_register(lua, "mapHash", getMapHash);
-
+    lua_register(lua, "race", getRace);
+    lua_register(lua, "force", getForceName);
+    lua_register(lua, "upgrading", isUpgrading);
+    lua_register(lua, "upgraded", getUpgrade);
+    lua_register(lua, "researched", isResearched);
+    lua_register(lua, "researching", isResearching);
+    lua_register(lua, "kills", getKills);
+    lua_register(lua, "deaths", getDeaths);
+    lua_register(lua, "units", getAllUnits);
+    lua_register(lua, "complete", getCompleteUnits);
+    lua_register(lua, "incomplete", getIncompleteUnits);
 
   } // constructor
 
@@ -148,6 +158,213 @@ namespace BWAI
     lua_pushnumber(l, BWAPI::Broodwar->getMapHash());
     return 1;
   }
+// ======================================== Get Race
+  int LUA::getRace(lua_State *l)
+  {
+    if (lua_gettop(l) != 0)
+    {
+      // error msg
+      return 0;
+    }
+    lua_pushnumber(l, BWAPI::Broodwar->self()->getRace().getID());
+    return 1;
+  }
+// ======================================== Get Force Name
+  int LUA::getForceName(lua_State *l)
+  {
+    if (lua_gettop(l) != 0)
+    {
+      // error msg
+      return 0;
+    }
+    lua_pushstring(l, BWAPI::Broodwar->self()->getForce()->getName().c_str());
+    return 1;
+  }
+// ======================================== Is Upgrading
+  int LUA::isUpgrading(lua_State *l)
+  {
+    u8 type = (u8)lua_tonumber(l, 1);
+    if (lua_gettop(l) != 1 || type > 60)
+    {
+      // error msg
+      return 0;
+    }
+    lua_pushnumber(l, BWAPI::Broodwar->self()->upgrading(type));
+    return 1;
+  }
+// ======================================== Get Upgrade Level
+  int LUA::getUpgrade(lua_State *l)
+  {
+    u8 type = (u8)lua_tonumber(l, 1);
+    if (lua_gettop(l) != 1 || type > 60)
+    {
+      // error msg
+      return 0;
+    }
+    lua_pushnumber(l, BWAPI::Broodwar->self()->upgradeLevel(type));
+    return 1;
+  }
+// ======================================== Is Researched
+  int LUA::isResearched(lua_State *l)
+  {
+    u8 type = (u8)lua_tonumber(l, 1);
+    if (lua_gettop(l) != 1 || type > 45)
+    {
+      // error msg
+      return 0;
+    }
+    lua_pushnumber(l, BWAPI::Broodwar->self()->researched(type));
+    return 1;
+  }
+// ======================================== Is Researching
+  int LUA::isResearching(lua_State *l)
+  {
+    u8 type = (u8)lua_tonumber(l, 1);
+    if (lua_gettop(l) != 1 || type > 45)
+    {
+      // error msg
+      return 0;
+    }
+    lua_pushnumber(l, BWAPI::Broodwar->self()->researching(type));
+    return 1;
+  }
+// ======================================== Kills
+  int LUA::getKills(lua_State *l)
+  {
+    u16 type;
+    switch(lua_gettop(l))
+    {
+    case 0:
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getKills(BW::UnitID::All));
+      break;
+    case 1:
+      type = (u16)lua_tonumber(l, 1);
+      if (type > 336)
+      {
+        // error msg
+        return 0;
+      }
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getKills(BW::UnitType((BW::UnitID::Enum)type)));
+      break;
+    default:
+      // error msg
+      return 0;
+    }
+    
+    return 1;
+  }
+// ======================================== Deaths
+  int LUA::getDeaths(lua_State *l)
+  {
+    u16 type;
+    switch(lua_gettop(l))
+    {
+    case 0:
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getDeaths(BW::UnitID::All));
+      break;
+    case 1:
+      type = (u16)lua_tonumber(l, 1);
+      if (type > 336)
+      {
+        // error msg
+        return 0;
+      }
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getDeaths(BW::UnitType((BW::UnitID::Enum)type)));
+      break;
+    default:
+      // error msg
+      return 0;
+    }
+    
+    return 1;
+  }
+// ======================================== Get all Units
+  int LUA::getAllUnits(lua_State *l)
+  {
+    u16 type;
+    switch(lua_gettop(l))
+    {
+    case 0:
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getAllUnits(BW::UnitID::All));
+      break;
+    case 1:
+      type = (u16)lua_tonumber(l, 1);
+      if (type > 336)
+      {
+        // error msg
+        return 0;
+      }
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getAllUnits(BW::UnitType((BW::UnitID::Enum)type)));
+      break;
+    default:
+      // error msg
+      return 0;
+    }
+    
+    return 1;
+  }
+// ======================================== Completed Units
+  int LUA::getCompleteUnits(lua_State *l)
+  {
+    u16 type;
+    u8 race;
+    switch(lua_gettop(l))
+    {
+    case 0:
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getCompletedUnits(BW::UnitID::All));
+      break;
+    case 1:
+      type = (u16)lua_tonumber(l, 1);
+      if (type > 336)
+      {
+        // error msg
+        return 0;
+      }
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getCompletedUnits(BW::UnitType((BW::UnitID::Enum)type)));
+      break;
+    case 2:
+      type = (u16)lua_tonumber(l, 1);
+      race = (u8)lua_tonumber(l, 2);
+      if (type > 336)
+      {
+        // error msg
+        return 0;
+      }
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getCompletedUnits((BW::UnitID::Enum)type, race));
+      break;
+    default:
+      // error msg
+      return 0;
+    }
+    
+    return 1;
+  }
+// ======================================== Incomplete units
+  int LUA::getIncompleteUnits(lua_State *l)
+  {
+    u16 type;
+    switch(lua_gettop(l))
+    {
+    case 0:
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getIncompleteUnits(BW::UnitID::All));
+      break;
+    case 1:
+      type = (u16)lua_tonumber(l, 1);
+      if (type > 336)
+      {
+        // error msg
+        return 0;
+      }
+      lua_pushnumber(l, BWAPI::Broodwar->self()->getIncompleteUnits(BW::UnitType((BW::UnitID::Enum)type)));
+      break;
+    default:
+      // error msg
+      return 0;
+    }
+    
+    return 1;
+  }
+
 
 } // namespace BWAI
 
