4d4656312d71ca380653b6fa9864532d9371b2e4
lowerlogic <lowerlogic@gmail.com>
2009-07-01 11:14:17 +0000

    Forgot to include the new files for set rally and patrol.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1022 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/BWAPI/Source/BWAPI/CommandFollow.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Command.h"
+#include <BW/PositionUnitTarget.h>
+namespace BWAPI { class UnitImpl; }
+namespace BWAPI
+{
+  /** BWAPI internal representation of Follow command. */
+  class CommandFollow : public Command
+  {
+    public :
+      /** 
+       * @param executor Executor of the Follow command (will be passed as executor 
+                to Command ancestor)
+       * @param target Target of the Follow command.
+       */
+      CommandFollow(UnitImpl* executor, UnitImpl* target);
+      virtual void execute();
+      BWAPI::CommandTypes::Enum getType();
+      std::string describe();
+    private :
+      UnitImpl* target;
+  };
+}
---
+++ bwapi/BWAPI/Source/BWAPI/CommandSetRally.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Command.h"
+#include <BW/PositionUnitTarget.h>
+namespace BWAPI { class UnitImpl; }
+namespace BWAPI
+{
+  /** BWAPI internal representation of Set Rally command. */
+  class CommandSetRally : public Command
+  {
+    public :
+      /** 
+       * @param executor Executor of the Set Rally command (will be passed as executor 
+                to Command ancestor)
+       * @param target Target of the Set Rally command.
+       */
+      CommandSetRally(UnitImpl* executor, BW::Position targetPosition);
+      CommandSetRally(UnitImpl* executor, UnitImpl* targetUnit);
+      virtual void execute();
+      BWAPI::CommandTypes::Enum getType();
+      std::string describe();
+    private :
+      BW::Position targetPosition;
+      UnitImpl* targetUnit;
+  };
+}
