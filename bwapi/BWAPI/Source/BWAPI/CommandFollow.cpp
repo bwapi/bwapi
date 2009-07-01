01560d722f2f2061596751c5afdf4d415625b65c
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2009-07-01 11:14:17 +0000

    Forgot to include the new files for set rally and patrol.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1022 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/BWAPI/Source/BWAPI/CommandFollow.cpp
@@ -0,0 +1,35 @@
+#include "CommandFollow.h"
+#include "UnitImpl.h"
+#include <BW/Unit.h>
+namespace BWAPI
+{
+  //---------------------------------------------- CONSTRUCTOR -----------------------------------------------
+  CommandFollow::CommandFollow(UnitImpl* executor, UnitImpl* target)
+  :Command(executor)
+  ,target(target)
+  {
+  }
+  //------------------------------------------------ EXECUTE -------------------------------------------------
+  void CommandFollow::execute()
+  {
+    for (unsigned int i = 0; i < this->executors.size(); i++)
+    {
+      if ((this->executors[i]->getType().canMove()))
+        {
+          this->executors[i]->getRawDataLocal()->orderID = BW::OrderID::Follow;
+          this->executors[i]->getRawDataLocal()->targetUnit = this->target->getOriginalRawData();
+        }
+    }
+  }
+  //------------------------------------------------ GET TYPE ------------------------------------------------
+  BWAPI::CommandTypes::Enum CommandFollow::getType()
+  {
+    return BWAPI::CommandTypes::Follow;
+  }
+  //----------------------------------------------------------------------------------------------------------
+  std::string CommandFollow::describe()
+  {
+	  return this->executors[0]->getName() + " followed unit";
+  }
+  //----------------------------------------------------------------------------------------------------------
+}
---
+++ bwapi/BWAPI/Source/BWAPI/CommandSetRally.cpp
@@ -0,0 +1,56 @@
+#include "CommandSetRally.h"
+#include "UnitImpl.h"
+#include <BW/Unit.h>
+namespace BWAPI
+{
+  //---------------------------------------------- CONSTRUCTOR -----------------------------------------------
+  CommandSetRally::CommandSetRally(UnitImpl* executor, BW::Position targetPosition)
+  :Command(executor)
+  ,targetPosition(targetPosition)
+  ,targetUnit(NULL)
+  {
+  }
+  //---------------------------------------------- CONSTRUCTOR -----------------------------------------------
+  CommandSetRally::CommandSetRally(UnitImpl* executor, UnitImpl* targetUnit)
+  :Command(executor)
+  ,targetPosition(0,0)
+  ,targetUnit(targetUnit)
+  {
+  }
+  //------------------------------------------------ EXECUTE -------------------------------------------------
+  void CommandSetRally::execute()
+  {
+    for (unsigned int i = 0; i < this->executors.size(); i++)
+    {
+      if ((this->executors[i]->getType().canProduce()))
+      {
+        if (this->targetUnit)
+        {
+          this->executors[i]->getRawDataLocal()->orderID = BW::OrderID::RallyPoint1;
+          this->executors[i]->getRawDataLocal()->rallyPsiProviderUnion.rally.rallyUnit = this->targetUnit->getOriginalRawData();
+        }
+        else
+        {
+          this->executors[i]->getRawDataLocal()->orderID = BW::OrderID::RallyPoint2;
+          this->executors[i]->getRawDataLocal()->rallyPsiProviderUnion.rally.rallyX = this->targetPosition.x;
+          this->executors[i]->getRawDataLocal()->rallyPsiProviderUnion.rally.rallyY = this->targetPosition.y;
+        }
+      }
+    }
+  }
+  //------------------------------------------------ GET TYPE ------------------------------------------------
+  BWAPI::CommandTypes::Enum CommandSetRally::getType()
+  {
+    return BWAPI::CommandTypes::SetRally;
+  }
+  //----------------------------------------------------------------------------------------------------------
+  std::string CommandSetRally::describe()
+  {
+    if (this->targetUnit)
+    {
+  	  return this->executors[0]->getName() + " set rally to unit";
+    }
+	  return this->executors[0]->getName() + " set rally to position";
+  }
+  //----------------------------------------------------------------------------------------------------------
+}
