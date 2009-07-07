59b0da104cbe3199e1de7c61617aa0acaead1846
quietdeath <quietdeath@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2009-07-07 23:09:32 +0000

    I found an algorithm which will fill the ellipse using disjunct rectangles which requires less drawing, but it isnt working yet. The ellipse is filled but not 100%. As I'm mainly in the university tomorrow I committed it if someone wants to take a look at it.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1065 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/ShapeEllipse.h
+++ bwapi/BWAPI/Source/BWAPI/ShapeEllipse.h
@@ -7,6 +7,8 @@ namespace BWAPI
   public :
     ShapeEllipse(CoordinateType::Enum ctype, int x, int y, int xrad, int yrad, int color, bool isSolid=false);
     virtual void draw();
+    virtual void fillRect(int x, int y, int w, int h);
+
   private :
     CoordinateType::Enum ctype;
     int x;
