b399cf868918c0666e5777955033c46f34fe9a9d
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2009-10-10 19:08:40 +0000

    Update sprite structure. Probably won't work because visibilityFlags is a different offset.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1365 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/Sprite.h
+++ bwapi/BWAPI/Source/BW/Sprite.h
@@ -2,11 +2,22 @@
 
 namespace BW
 {
-  struct Sprite
+  struct CSprite
   {
-    u8 _1[12];
+    CSprite* prev;
+    CSprite* next;
+    u16 spriteID;
+    u8 color;
+    u8 unknown1;
+    u8 unknown2;
+    u8 elevationLevel;
     u8 visibilityFlags;
-    u8 _2[23];
+    u8 selectionTimer;
+    u32 index;
+    Position position;
+    u32 CImage_underlay;
+    u32 CImage_mainGraphic;
+    u32 CImage_overlay;
   };
 };
 
