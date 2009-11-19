1d5f15dcae3863dfeb004e24b95e7014e57089e5
lowerlogic <lowerlogic@gmail.com>
2009-11-19 20:02:42 +0000

    fixed unit position target
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1510 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/PositionUnitTarget.cpp
+++ bwapi/BWAPI/Source/BW/PositionUnitTarget.cpp
@@ -11,8 +11,8 @@ namespace BW
   PositionUnitTarget::PositionUnitTarget(const UnitTarget& target)
       : target(target)
   {
-    u16 index = 336 * (target.getTarget() & 0x7FF - 1);
-    Unit* unit = (Unit*)((int)BW::BWDATA_UnitNodeTable + index);
+    int index = 336 * ((target.getTarget() & 0x7FF) - 1);
+    Unit* unit = (Unit*)((int)BWDATA_UnitNodeTable + index);
     this->position = unit->position;
   }
   //----------------------------------------------------------------------------------------------------------
