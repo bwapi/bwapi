2a3233ba5f797b41ff842be8fc670e0e34a987b2
Adam Heinermann <aheinerm@gmail.com>
2009-11-25 15:19:04 +0000

    Forgot these includes. Change backslash to forward slash for compatibility.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1649 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
+++ bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
@@ -1,6 +1,6 @@
 #define WIN32_LEAN_AND_MEAN   // Exclude rarely-used stuff from Windows headers
 
-#include "..\\..\\svnrev.h"
+#include "../../svnrev.h"
 #include "GameImpl.h"
 
 #include <stdio.h>
