96c50ca87da9e664a5f13cbe1777376e97b96367
lowerlogic <lowerlogic@gmail.com>
2009-12-03 02:33:18 +0000

    Added UnitCommand struct w/ CommandID enum. Also added a function that executes the command in Engine.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1745 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/BWAPITypes/UnitCommands.h
@@ -0,0 +1,59 @@
+#pragma once
+namespace BWAPI
+{
+  namespace CommandID
+  {
+    enum Enum
+    {
+      AttackPosition,
+      AttackUnit,
+      RightClickPosition,
+      RightClickUnit,
+      Train,
+      Build,
+      BuildAddon,
+      Research,
+      Upgrade,
+      Stop,
+      HoldPosition,
+      Patrol,
+      Follow,
+      SetRallyPosition,
+      SetRallyUnit,
+      Repair,
+      Morph,
+      Burrow,
+      Unburrow,
+      Siege,
+      Unsiege,
+      Cloak,
+      Decloak,
+      Lift,
+      Land,
+      Load,
+      Unload,
+      UnloadAll,
+      UnloadAllPosition,
+      CancelConstruction,
+      HaltConstruction,
+      CancelMorph,
+      CancelTrain,
+      CancelTrainSlot,
+      CancelAddon,
+      CancelResearch,
+      CancelUpgrade,
+      UseTech,
+      UseTechPosition,
+      UseTechUnit
+    };
+  }
+  struct UnitCommand
+  {
+    CommandID::Enum commandID;
+    int unitID;
+    int x; //x position or tile position, depending on order type
+    int y; //y position or tile position, depending on order type
+    int targetID; //target unit ID, if applicable
+    int specialID; //tech type, upgrade type, or unit type, depending on command type
+  };
+}
