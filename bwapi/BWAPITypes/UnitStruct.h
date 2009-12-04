c7199441b10101dac3a323e26533bbeb8093612f
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2009-12-04 07:49:32 +0000

    Added Unit::is___ commands.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1759 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPITypes/UnitStruct.h
+++ bwapi/BWAPITypes/UnitStruct.h
@@ -29,6 +29,50 @@ namespace BWAPI
     //PlayerStruct* getPlayer;
     int getType;
     int getHitPoints;
+
+    //todo: move some of these to fully observable
+    bool exists;
+    bool isAccelerating;
+    bool isBeingConstructed;
+    bool isBeingGathered;
+    bool isBeingHealed;
+    bool isBlind;
+    bool isBraking;
+    bool isBurrowed;
+    bool isCarryingGas;
+    bool isCarryingMinerals;
+    bool isCloaked;
+    bool isCompleted;
+    bool isConstructing;
+    bool isDefenseMatrixed;
+    bool isEnsnared;
+    bool isFollowing;
+    bool isGatheringGas;
+    bool isGatheringMinerals;
+    bool isHallucination;
+    bool isIdle;
+    bool isIrradiated;
+    bool isLifted;
+    bool isLoaded;
+    bool isLockedDown;
+    bool isMaelstrommed;
+    bool isMorphing;
+    bool isMoving;
+    bool isParasited;
+    bool isPatrolling;
+    bool isPlagued;
+    bool isRepairing;
+    bool isResearching;
+    bool isSelected;
+    bool isSieged;
+    bool isStartingAttack;
+    bool isStasised;
+    bool isStimmed;
+    bool isTraining;
+    bool isUnderStorm;
+    bool isUnpowered;
+    bool isUpgrading;
+    bool isVisible;
   };
   struct StateFullyObservable : StateVisible
   {
