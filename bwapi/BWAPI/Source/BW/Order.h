660f00cbbf961304c15cfe8b895e82cb330e582f
lowerlogic <lowerlogic@gmail.com>
2009-12-11 02:45:06 +0000

    Added BW::Order structure as described by Echo 8 in the forum.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1784 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/Order.h
+++ bwapi/BWAPI/Source/BW/Order.h
@@ -1,6 +1,17 @@
 #pragma once
 
+namespace BW { struct Unit; };
 namespace BW /** @todo Investigate*/
 {
-  struct Order;
+#pragma pack(1)
+  struct Order
+  {
+    BW::Order*   previousOrder;
+    BW::Order*   nextOrder;
+    u16          orderID;
+    u16          unknown_0xE4;
+    BW::Position targetPosition;
+    BW::Unit*    targetUnit;
+  };
+#pragma pack()
 };
