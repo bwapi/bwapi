b12ac0dea4a40bb6419051cac3960ca3e1eed7a6
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2009-12-20 06:42:53 +0000

    Applied my changes since r1708.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1812 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/Order.h
+++ bwapi/BWAPI/Source/BW/Order.h
@@ -1,6 +1,18 @@
 #pragma once
 
+namespace BW { struct Unit; };
+#include "UnitType.h"
 namespace BW /** @todo Investigate*/
 {
-  struct Order;
+#pragma pack(1)
+  struct Order
+  {
+    BW::Order*   previousOrder;
+    BW::Order*   nextOrder;
+    u16          orderID;
+    UnitType     unitType;
+    BW::Position targetPosition;
+    BW::Unit*    targetUnit;
+  };
+#pragma pack()
 };
