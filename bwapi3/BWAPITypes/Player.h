c874c76f2ad7359b2525c5907e52eddf2a65ebff
google@teabix.com <google@teabix.com>
2009-12-30 00:28:45 +0000

    - fixed cheat recognition
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1853 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi3/BWAPITypes/Player.h
+++ bwapi3/BWAPITypes/Player.h
@@ -7,7 +7,8 @@ namespace BWAPI
   struct Player
   {
     ForceId force;
-    s32 mineralsLocal;
-    s32 gasLocal;
+    s32 minerals;
+    s32 gas;
+    char name[25];  // size as BW memory
   };
 }
