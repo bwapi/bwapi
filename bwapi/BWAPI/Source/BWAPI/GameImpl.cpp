bwapi/bwapi

commit 97129dd1dd274cbe74a8cb36176c176f1f26705a

fixed a bug with unit sets obtained during the onUnitCreate and onNukeDetect callbacks.
git-svn-id: https://bwapi.googlecode.com/svn/trunk@1868 273ecf2c-f24b-0410-b6a7-e9298bb4873f

bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
@@ -712,14 +712,15 @@ namespace BWAPI
         Broodwar->drawCircle(BWAPI::CoordinateType::Map, curritem->pos_x, curritem->pos_y, 4, BWAPI::Colors::White, false);
       }
 */
-
+      this->unitsToBeAdded.clear();
+      std::list<Position> detectedNukes;
       foreach (UnitImpl* i, unitList)
       {
         if (this->units.find(i) == this->units.end())
         {
           i->alive=true;
           this->units.insert(i);
-          this->onAddUnit(i);
+          this->unitsToBeAdded.insert(i);
         }
       }
       foreach (UnitImpl* i, units)
@@ -740,9 +741,9 @@ namespace BWAPI
           if (this->client)
           {
             if (this->isFlagEnabled(Flag::CompleteMapInformation) || this->isVisible(target.x()/32,target.y()/32))
-              this->client->onNukeDetect(target);
+              detectedNukes.push_back(target);
             else
-              this->client->onNukeDetect(Positions::Unknown);
+              detectedNukes.push_back(Positions::Unknown);
           }
         }
       }
@@ -761,6 +762,10 @@ namespace BWAPI
           this->commandBuffer[i][j]->execute();
 
       this->updateUnits();
+      for(std::list<Position>::iterator i=detectedNukes.begin();i!=detectedNukes.end();i++)
+      {
+        this->client->onNukeDetect(*i);
+      }
 
       for (unsigned int i = 0; i < this->shapes.size(); i++)
         delete this->shapes[i];
@@ -1215,6 +1220,7 @@ namespace BWAPI
     this->geysers.clear();
     this->neutralUnits.clear();
     this->myPylons.clear();
+    this->unitsToBeAdded.clear();
 
     this->staticMinerals.clear();
     this->staticGeysers.clear();
@@ -1592,6 +1598,9 @@ namespace BWAPI
         }
       }
     }
+    foreach (BWAPI::UnitImpl* i, unitsToBeAdded)
+      if (this->client)
+        this->client->onUnitCreate(i);
 
     /* Pass all renegade units to the AI client */
     foreach (BWAPI::UnitImpl* i, renegadeUnits)

bwapi/BWAPI/Source/BWAPI/GameImpl.h
@@ -235,6 +235,7 @@ namespace BWAPI
       std::set<BWAPI::Unit*> minerals;
       std::set<BWAPI::Unit*> geysers;
       std::set<BWAPI::Unit*> neutralUnits;
+      std::set<BWAPI::UnitImpl*> unitsToBeAdded;
       std::list<BWAPI::UnitImpl*> myPylons;
       Util::RectangleArray<std::set<Unit*> > unitsOnTileData;
 

commit 135a03fd71b94a0a8e2a2bebbf56e22893a8a7fd

fixed something i accidentally broke in r1868.
git-svn-id: https://bwapi.googlecode.com/svn/trunk@1910 273ecf2c-f24b-0410-b6a7-e9298bb4873f

bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
@@ -1744,7 +1744,8 @@ namespace BWAPI
     }
     foreach (BWAPI::UnitImpl* i, unitsToBeAdded)
       if (this->client)
-        this->client->onUnitCreate(i);
+        if (i->canAccess())
+          this->client->onUnitCreate(i);
 
     /* Pass all renegade units to the AI client */
     foreach (BWAPI::UnitImpl* i, renegadeUnits)