eeab68ce3ea454b72df7bdd254288d575ba02a81
google@teabix.com <google@teabix.com>
2010-01-02 23:05:00 +0000

    IP fix
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1939 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi3/BWAPI2_Interface/all.cmd
+++ bwapi3/BWAPI2_Interface/all.cmd
@@ -1,6 +1,5 @@
 @echo off
 cd "..\Interface Packer"
-del include\BWAPI2\*.* /q
 del include\BWAPI\*.* /q
 "..\debug\interface packer.exe" "BWAPI1.ini"
 rem @pause
--- bwapi3/ConsoleAIModule/all.cmd
+++ bwapi3/ConsoleAIModule/all.cmd
@@ -1,7 +1,5 @@
 @echo off
 cd "..\Interface Packer"
 del include\BWAPI2\*.* /q
-del include\BWAPI\*.* /q
-rem "..\debug\interface packer.exe" "BWAPI1.ini"
 "..\debug\interface packer.exe" "BWAPI2.ini"
 rem @pause
