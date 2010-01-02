f320a59d9717b37f4f66fa00581ee31042ace419
google@teabix.com <google@teabix.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-01-02 21:10:47 +0000

    missing file sorry o.o
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1933 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi3/BWAPI2_Interface/Source/dllmain.cpp
@@ -0,0 +1,23 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+
+#include <windows.h>
+
+#include "BWAPI2\BWAPI.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+      BWAPI2::BWAPI_init();
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
