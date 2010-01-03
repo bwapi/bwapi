e4d9b3c837e2d14cd9431063210912ef87b45203
lowerlogic <lowerlogic@gmail.com>
2010-01-03 02:19:48 +0000

    some initial changes for BWAPI2::Player and BWAPI2::Force
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1946 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi3/BWAPI2_Interface/Source/BWAPI2/Force.h
+++ bwapi3/BWAPI2_Interface/Source/BWAPI2/Force.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "BWAPI2Interface.h"
 
+IP_STRIP
+
 #include <set>
 #include <string>
 
-namespace BWAPI
+namespace BWAPI2
 {
   class Player;
   class Force
   {
-    public :
-      virtual std::string getName() const = 0;
-      virtual std::set<Player*> getPlayers() const = 0;
+  public :
+    BWAPI2_EXPORT std::string getName() const;
+    BWAPI2_EXPORT std::set<Player*> getPlayers() const;
   };
 }
