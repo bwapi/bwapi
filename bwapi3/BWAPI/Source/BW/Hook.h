b973fffe3dc6e8c9437a546cf7f738a12a3356a5
google@teabix.com <google@teabix.com>
2010-01-10 15:10:06 +0000

    BWAPIv3: - moved drawing operations on a boosted BW::drawScanLine core function, away from BW::drawBox
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1983 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi3/BWAPI/Source/BW/Hook.h
+++ bwapi3/BWAPI/Source/BW/Hook.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Util\Types.h>
+#include <memory.h>
 
 namespace BW
 {
@@ -18,19 +19,38 @@ namespace BW
   extern void drawBox(int x, int y, int w, int h, int color);
   extern void drawText(int x, int y, const char* ptext);
   //extern void drawDot(int x, int y, int color);
+
+
+  struct ScreenBufferData
+  {
+    u16 width;
+    u16 height;
+    BYTE* buffer;
+  };
+  static const ScreenBufferData** const screenBufferData = (const ScreenBufferData**)0x006CF4A8;
+
   static void inline drawDot(int x, int y, int color)
   {
 //    drawBox(x, y, 1, 1, color);
-    struct ScreenBufferData
-    {
-      u16 width;
-      u16 height;
-      BYTE* buffer;
-    };
-    static const ScreenBufferData** screenBufferData = (const ScreenBufferData**)0x006CF4A8;
 
     if(x >= 0 && x < 640 && y >= 0 && y < 480)
       (*screenBufferData)->buffer[y*640 + x] = color;
+  }
+  static void inline drawScanLine(int x, int y, int width, int color)
+  {
+    if(x+width >= 0 && x < 640 && y >= 0 && y < 480)
+    {
+      if(x + width > 640)
+      {
+        width = 640 - x;
+      }
+      if(x < 0)
+      {
+        width += x;
+        x = 0;
+      }
+      memset(&((*screenBufferData)->buffer[y*640 + x]), color, width);
+    }
   }
 
   extern void printEx(s32 pID, const char* text, ...);
