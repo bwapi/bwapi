a3753c8d2f636204a72af800e6957e9b279fbc5d
google@teabix.com <google@teabix.com>
2010-01-10 14:29:32 +0000

    BWAPIv3: - boosted BW::drawDot. drawing circles should have gotten as fast as drawing rechtangles
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@1982 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi3/BWAPI/Source/BW/Hook.h
+++ bwapi3/BWAPI/Source/BW/Hook.h
@@ -17,9 +17,20 @@ namespace BW
   // draw functions
   extern void drawBox(int x, int y, int w, int h, int color);
   extern void drawText(int x, int y, const char* ptext);
+  //extern void drawDot(int x, int y, int color);
   static void inline drawDot(int x, int y, int color)
   {
-    drawBox(x, y, 1, 1, color);
+//    drawBox(x, y, 1, 1, color);
+    struct ScreenBufferData
+    {
+      u16 width;
+      u16 height;
+      BYTE* buffer;
+    };
+    static const ScreenBufferData** screenBufferData = (const ScreenBufferData**)0x006CF4A8;
+
+    if(x >= 0 && x < 640 && y >= 0 && y < 480)
+      (*screenBufferData)->buffer[y*640 + x] = color;
   }
 
   extern void printEx(s32 pID, const char* text, ...);
