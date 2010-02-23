8d8665a820cc9ad830da045faca56bfeb817a499
google@teabix.com <google@teabix.com>
2010-02-23 15:38:05 +0000

    fixed the "quickfix for version control related problems" like in issue 198
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2107 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi3/revisionUpdate.bat
+++ bwapi3/revisionUpdate.bat
@@ -1,2 +1,8 @@
-@SubWCRev.exe . svnrev_template.h svnrev.h
-@pause
+@echo off
+if not exist svnrev.h (
+SubWCRev.exe . svnrev_template.h svnrev.h
+pause
+) else (
+echo svnrev.h already exists, nothing to do here
+pause
+)
