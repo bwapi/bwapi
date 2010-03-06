04de4fb487e00a5327c671d683073d90f1fc9b9f
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-03-06 20:13:07 +0000

    Added macros for repetitive attemptAccess calls. Some spacing, minor tweaks. Attempt to fix issue 188. Will grab a subunit's cooldown values if it exists instead of the base unit. Please verify if this works.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2109 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/UnitImpl.h
+++ bwapi/BWAPI/Source/BWAPI/UnitImpl.h
@@ -13,6 +13,13 @@
 #include <BW/TilePosition.h>
 #include <BW/OrderID.h>
 
+#define checkAccessBool() if (!this->attemptAccess()) return false
+#define checkAccessInt() if (!this->attemptAccess()) return 0
+#define checkAccessPosition() if (!this->attemptAccess()) return BWAPI::Positions::Unknown
+#define checkAccessTilePosition() if (!this->attemptAccess()) return BWAPI::TilePositions::Unknown
+#define checkAccessDouble() if (!this->attemptAccess()) return std::numeric_limits<double>::infinity()
+#define checkAccessPointer() if (!this->attemptAccess()) return NULL
+
 namespace Util  { class Logger; }
 namespace BW    { class Position; };
 namespace BW    { struct Unit; };
