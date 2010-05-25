bdb1c9f6cc4a8ef3e442e7c62e4b0ab9f307ee31
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-05-25 20:59:23 +0000

    Fixed a bug where onUnitDestroy was being called for invisible units with complete map info disabled.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2255 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/UnitImpl.cpp
+++ bwapi/BWAPI/Source/BWAPI/UnitImpl.cpp
@@ -2590,8 +2590,6 @@ namespace BWAPI
       if (BroodwarImpl.inUpdate == true)
         return true;
 
-      if (this->getRawDataLocal()->orderID == BW::OrderID::Die)
-        return true;
     }
     return false;
   }
