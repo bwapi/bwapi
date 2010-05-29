d7763b022a114a99c4b650b525b0ffe5910f5d52
lowerlogic <lowerlogic@gmail.com>
2010-05-29 21:59:06 +0000

    updated BWAPI::GameType to match BW::GameType numbers and updated GameImpl to use setSelectedByValue for game type. Tested and works for single player and udp lan games.
    Changed changeRace to use setSelectedIndex since BWAPI::Random.getID() == 3.
    
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2289 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPILIB/Source/GameType.cpp
+++ bwapi/BWAPILIB/Source/GameType.cpp
@@ -6,53 +6,59 @@
 namespace BWAPI
 {
   bool initializingGameType = true;
-  std::string gameTypeName[14];
+  std::string gameTypeName[35];
   std::map<std::string, GameType> gameTypeMap;
   std::set< GameType > gameTypeSet;
   namespace GameTypes
   {
-    const GameType Top_vs_Bottom(0);
-    const GameType Melee(1);
-    const GameType Free_For_All(2);
-    const GameType One_on_One(3);
-    const GameType Use_Map_Settings(4);
+    const GameType Melee(2);
+    const GameType Free_For_All(3);
+    const GameType One_on_One(4);
     const GameType Capture_The_Flag(5);
     const GameType Greed(6);
     const GameType Slaughter(7);
     const GameType Sudden_Death(8);
-    const GameType Team_Melee(9);
-    const GameType Team_Free_For_All(10);
-    const GameType Team_Capture_The_Flag(11);
-    const GameType None(12);
-    const GameType Unknown(13);
+    const GameType Ladder(9);
+    const GameType Use_Map_Settings(10);
+    const GameType Team_Melee(11);
+    const GameType Team_Free_For_All(12);
+    const GameType Team_Capture_The_Flag(13);
+    const GameType Top_vs_Bottom(15);
+    const GameType Pro_Gamer_League(32);
+    const GameType None(33);
+    const GameType Unknown(34);
     void init()
     {
-      gameTypeName[Top_vs_Bottom.getID()] = "Top vs Bottom";
       gameTypeName[Melee.getID()] = "Melee";
       gameTypeName[Free_For_All.getID()] = "Free For All";
       gameTypeName[One_on_One.getID()] = "One on One";
-      gameTypeName[Use_Map_Settings.getID()] = "Use Map Settings";
       gameTypeName[Capture_The_Flag.getID()] = "Capture The Flag";
       gameTypeName[Greed.getID()] = "Greed";
       gameTypeName[Slaughter.getID()] = "Slaughter";
       gameTypeName[Sudden_Death.getID()] = "Sudden Death";
+      gameTypeName[Ladder.getID()] = "Ladder";
+      gameTypeName[Use_Map_Settings.getID()] = "Use Map Settings";
       gameTypeName[Team_Melee.getID()] = "Team Melee";
       gameTypeName[Team_Free_For_All.getID()] = "Team Free For All";
       gameTypeName[Team_Capture_The_Flag.getID()] = "Team Capture The Flag";
+      gameTypeName[Top_vs_Bottom.getID()] = "Top vs Bottom";
+      gameTypeName[Pro_Gamer_League.getID()] = "Pro Gamer League";
       gameTypeName[None.getID()] = "None";
       gameTypeName[Unknown.getID()] = "Unknown";
-      gameTypeSet.insert(Top_vs_Bottom);
       gameTypeSet.insert(Melee);
       gameTypeSet.insert(Free_For_All);
       gameTypeSet.insert(One_on_One);
-      gameTypeSet.insert(Use_Map_Settings);
       gameTypeSet.insert(Capture_The_Flag);
       gameTypeSet.insert(Greed);
       gameTypeSet.insert(Slaughter);
       gameTypeSet.insert(Sudden_Death);
+      gameTypeSet.insert(Ladder);
+      gameTypeSet.insert(Use_Map_Settings);
       gameTypeSet.insert(Team_Melee);
       gameTypeSet.insert(Team_Free_For_All);
       gameTypeSet.insert(Team_Capture_The_Flag);
+      gameTypeSet.insert(Top_vs_Bottom);
+      gameTypeSet.insert(Pro_Gamer_League);
       gameTypeSet.insert(None);
       gameTypeSet.insert(Unknown);
       for(std::set<GameType>::iterator i = gameTypeSet.begin(); i != gameTypeSet.end(); i++)
@@ -77,7 +83,7 @@ namespace BWAPI
     this->id = id;
     if (!initializingGameType)
     {
-      if (id < 0 || id >= 14 || gameTypeName[id].length() == 0)
+      if (id < 0 || id >= 35 || gameTypeName[id].length() == 0)
       {
         this->id = GameTypes::Unknown.id;
       }
