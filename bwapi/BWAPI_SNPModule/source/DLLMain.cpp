33fc72e0dac42c1a754709663cdf36ba756e01c2
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-05-30 18:07:27 +0000

    SNP research
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2302 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI_SNPModule/source/DLLMain.cpp
+++ bwapi/BWAPI_SNPModule/source/DLLMain.cpp
@@ -22,7 +22,7 @@ struct netFunctions
 {
   DWORD dwSize;
   void  *fxn_0;
-  BOOL  (__stdcall *Cleanup)();
+  bool  (__stdcall *Cleanup)();
   void  *fxn_2;
   void  *fxn_3;
   void  *fxn_4;
@@ -37,8 +37,8 @@ struct netFunctions
   void  *fxn_13;
   void  *fxn_14;
   void  *fxn_15;
-  void  *fxn_16;
-  void  *fxn_17;
+  bool (__stdcall *LeaveGame)();
+  bool (__stdcall *InitializeUser)();
   void  *fxn_18;
   void  *fxn_19;
   void  *fxn_20;
@@ -47,7 +47,7 @@ struct netFunctions
   void  *fxn_23;
   void  *fxn_24;
   void  *fxn_25;
-  void  *fxn_26;
+  bool (__stdcall *GetLeagueName)(char *pszDest, DWORD dwSize);
 };
 
 struct netStruct
@@ -60,13 +60,13 @@ struct netStruct
 } networks[] = {
   { "Local PC", 'LOCL', "BWAPI 1.16.1 r" SVN_REV_STR "\n\nConnect multiple instances of Starcraft together on the same PC.",
     { sizeof(caps), 0x20000000, 0x200, 0x10, 0x100, 100000, 50, 8, 2},
-    { sizeof(netFunctions), &fxn1, &Cleanup, &fxn3, &fxn4, 
-      &fxn5, &fxn6, &fxn7, &fxn8, 
-      &fxn9, &fxn10, &fxn11, &fxn12, 
-      &fxn13, &fxn14, &fxn15, &CreateGame, 
-      &LeaveGame, &fxn18, &fxn19, NULL, 
-      NULL, NULL, NULL, NULL, 
-      NULL, NULL, &BeginTextMessage}
+    { sizeof(netFunctions), &fxn0, &Cleanup, &fxn2, &fxn3,
+      &fxn4, &fxn5, &InitializeModule, &fxn7,
+      &fxn8, &fxn9, &fxn10, &fxn11,
+      &fxn12, &fxn13, &fxn14, &CreateGame,
+      &LeaveGame, &InitializeUser, &fxn18, &fxn19,
+      &fxn20, &fxn21, &fxn22, &fxn23,
+      &fxn24, &LeagueLogout, &GetLeagueName}
   }
 };
 
