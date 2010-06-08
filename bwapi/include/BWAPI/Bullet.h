018d419217e5461e26a5c0db43c4cf65829e849e
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-06-08 00:02:51 +0000

    simplified Bullet.h a bit
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2365 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/include/BWAPI/Bullet.h
+++ bwapi/include/BWAPI/Bullet.h
@@ -12,17 +12,17 @@ namespace BWAPI
     public:
       virtual int getID() const = 0;
       virtual Player* getPlayer() const = 0;
-      virtual BWAPI::BulletType getType() const = 0;
-      virtual BWAPI::Unit* getSource() const = 0;
-      virtual BWAPI::Position getPosition() const = 0;
+      virtual BulletType getType() const = 0;
+      virtual Unit* getSource() const = 0;
+      virtual Position getPosition() const = 0;
       virtual double getAngle() const = 0;
       virtual double getVelocityX() const = 0;
       virtual double getVelocityY() const = 0;
-      virtual BWAPI::Unit* getTarget() const = 0;
-      virtual BWAPI::Position getTargetPosition() const = 0;
+      virtual Unit* getTarget() const = 0;
+      virtual Position getTargetPosition() const = 0;
       virtual int getRemoveTimer() const = 0;
       virtual bool exists() const = 0;
       virtual bool isVisible() const = 0;
-      virtual bool isVisible(BWAPI::Player* player) const = 0;
+      virtual bool isVisible(Player* player) const = 0;
   };
 }
