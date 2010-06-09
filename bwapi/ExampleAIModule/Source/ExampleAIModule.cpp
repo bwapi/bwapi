6a508f15957c898f1ebc8a5e935942b3e96a1dc7
lowerlogic <lowerlogic@gmail.com>
2010-06-09 11:17:02 +0000

    updated Example AI Module
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2405 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/ExampleAIModule/Source/ExampleAIModule.cpp
+++ bwapi/ExampleAIModule/Source/ExampleAIModule.cpp
@@ -1,5 +1,11 @@
 #include "ExampleAIModule.h"
 using namespace BWAPI;
+/*
+bool analyzed;
+bool analysis_just_finished;
+BWTA::Region* home;
+BWTA::Region* enemy_base;
+*/
 void ExampleAIModule::onStart()
 {
   Broodwar->sendText("Hello world!");
@@ -8,7 +14,14 @@ void ExampleAIModule::onStart()
   Broodwar->enableFlag(Flag::UserInput);
   // Uncomment to enable complete map information
   //Broodwar->enableFlag(Flag::CompleteMapInformation);
-
+  /*
+  //read map information into BWTA so terrain analysis can be done in another thread
+  BWTA::readMap();
+  analyzed=false;
+  analysis_just_finished=false;
+  */
+  show_bullets = false;
+  show_visibility_data = false;
 
   if (Broodwar->isReplay())
   {
@@ -70,10 +83,104 @@ void ExampleAIModule::onEnd(bool isWinner)
 }
 void ExampleAIModule::onFrame()
 {
+  if (show_visibility_data)
+    drawVisibilityData();
+
+  if (show_bullets)
+    drawBullets();
+
   if (Broodwar->isReplay())
     return;
 
   drawStats();
+  /*
+  if (analyzed && Broodwar->getFrameCount()%30==0)
+  {
+    //order one of our workers to guard our chokepoint.
+    for(std::set<Unit*>::const_iterator i=Broodwar->self()->getUnits().begin();i!=Broodwar->self()->getUnits().end();i++)
+    {
+      if ((*i)->getType().isWorker())
+      {
+        //get the chokepoints linked to our home region
+        std::set<BWTA::Chokepoint*> chokepoints= home->getChokepoints();
+        double min_length=10000;
+        BWTA::Chokepoint* choke=NULL;
+
+        //iterate through all chokepoints and look for the one with the smallest gap (least width)
+        for(std::set<BWTA::Chokepoint*>::iterator c=chokepoints.begin();c!=chokepoints.end();c++)
+        {
+          double length=(*c)->getWidth();
+          if (length<min_length || choke==NULL)
+          {
+            min_length=length;
+            choke=*c;
+          }
+        }
+
+        //order the worker to move to the center of the gap
+        (*i)->rightClick(choke->getCenter());
+        break;
+      }
+    }
+  }
+  if (analyzed)
+    drawTerrainData();
+
+  if (analysis_just_finished)
+  {
+    Broodwar->printf("Finished analyzing map.");
+    analysis_just_finished=false;
+  }
+  */
+}
+
+bool ExampleAIModule::onSendText(std::string text)
+{
+  if (text=="/show bullets")
+  {
+    show_bullets = !show_bullets;
+  } else if (text=="/show players")
+  {
+    showPlayers();
+    return false;
+  } else if (text=="/show forces")
+  {
+    showForces();
+    return false;
+  } else if (text=="/show visibility")
+  {
+    show_visibility_data=!show_visibility_data;
+  } /* else if (text=="/analyze")
+  {
+    if (analyzed == false)
+    {
+      Broodwar->printf("Analyzing map... this may take a minute");
+      CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)AnalyzeThread, NULL, 0, NULL);
+    }
+    return false;
+  } */ else
+  {
+    Broodwar->printf("You typed '%s'!",text.c_str());
+  }
+  return true;
+}
+
+void ExampleAIModule::onReceiveText(BWAPI::Player* player, std::string text)
+{
+  Broodwar->sendText("%s said '%s'", player->getName().c_str(), text.c_str());
+}
+
+void ExampleAIModule::onPlayerLeft(BWAPI::Player* player)
+{
+  Broodwar->sendText("%s left the game.",player->getName().c_str());
+}
+
+void ExampleAIModule::onNukeDetect(BWAPI::Position target)
+{
+  if (target!=Positions::Unknown)
+    Broodwar->printf("Nuclear Launch Detected at (%d,%d)",target.x(),target.y());
+  else
+    Broodwar->printf("Nuclear Launch Detected");
 }
 
 void ExampleAIModule::onUnitCreate(BWAPI::Unit* unit)
@@ -93,6 +200,7 @@ void ExampleAIModule::onUnitCreate(BWAPI::Unit* unit)
     }
   }
 }
+
 void ExampleAIModule::onUnitDestroy(BWAPI::Unit* unit)
 {
   if (!Broodwar->isReplay())
@@ -116,50 +224,49 @@ void ExampleAIModule::onUnitMorph(BWAPI::Unit* unit)
     }
   }
 }
+
 void ExampleAIModule::onUnitShow(BWAPI::Unit* unit)
 {
   if (!Broodwar->isReplay())
     Broodwar->sendText("A %s [%x] has been spotted at (%d,%d)",unit->getType().getName().c_str(),unit,unit->getPosition().x(),unit->getPosition().y());
 }
+
 void ExampleAIModule::onUnitHide(BWAPI::Unit* unit)
 {
   if (!Broodwar->isReplay())
     Broodwar->sendText("A %s [%x] was last seen at (%d,%d)",unit->getType().getName().c_str(),unit,unit->getPosition().x(),unit->getPosition().y());
 }
+
 void ExampleAIModule::onUnitRenegade(BWAPI::Unit* unit)
 {
   if (!Broodwar->isReplay())
     Broodwar->sendText("A %s [%x] is now owned by %s",unit->getType().getName().c_str(),unit,unit->getPlayer()->getName().c_str());
 }
-void ExampleAIModule::onPlayerLeft(BWAPI::Player* player)
-{
-  Broodwar->sendText("%s left the game.",player->getName().c_str());
-}
-void ExampleAIModule::onNukeDetect(BWAPI::Position target)
-{
-  if (target!=Positions::Unknown)
-    Broodwar->printf("Nuclear Launch Detected at (%d,%d)",target.x(),target.y());
-  else
-    Broodwar->printf("Nuclear Launch Detected");
-}
 
-bool ExampleAIModule::onSendText(std::string text)
+void ExampleAIModule::onSaveGame(std::string gameName)
 {
-  if (text=="/show players")
+  Broodwar->printf("The game was saved to \"%s\".", gameName.c_str());
+}
+/*
+DWORD WINAPI AnalyzeThread()
+{
+  BWTA::analyze();
+
+  //self start location only available if the map has base locations
+  if (BWTA::getStartLocation(BWAPI::Broodwar->self())!=NULL)
   {
-    showPlayers();
-    return false;
-  } else if (text=="/show forces")
-  {
-    showForces();
-    return false;
-  } else
-  {
-    Broodwar->printf("You typed '%s'!",text.c_str());
+    home       = BWTA::getStartLocation(BWAPI::Broodwar->self())->getRegion();
   }
-  return true;
+  //enemy start location only available if Complete Map Information is enabled.
+  if (BWTA::getStartLocation(BWAPI::Broodwar->enemy())!=NULL)
+  {
+    enemy_base = BWTA::getStartLocation(BWAPI::Broodwar->enemy())->getRegion();
+  }
+  analyzed   = true;
+  analysis_just_finished = true;
+  return 0;
 }
-
+*/
 void ExampleAIModule::drawStats()
 {
   std::set<Unit*> myUnits = Broodwar->self()->getUnits();
@@ -181,6 +288,100 @@ void ExampleAIModule::drawStats()
   }
 }
 
+void ExampleAIModule::drawBullets()
+{
+  std::set<Bullet*> bullets = Broodwar->getBullets();
+  for(std::set<Bullet*>::iterator i=bullets.begin();i!=bullets.end();i++)
+  {
+    Position p=(*i)->getPosition();
+    double velocityX = (*i)->getVelocityX();
+    double velocityY = (*i)->getVelocityY();
+    if ((*i)->getPlayer()==Broodwar->self())
+    {
+      Broodwar->drawLineMap(p.x(),p.y(),p.x()+(int)velocityX,p.y()+(int)velocityY,Colors::Green);
+      Broodwar->drawTextMap(p.x(),p.y(),"\x07%s",(*i)->getType().getName().c_str());
+    }
+    else
+    {
+      Broodwar->drawLineMap(p.x(),p.y(),p.x()+(int)velocityX,p.y()+(int)velocityY,Colors::Red);
+      Broodwar->drawTextMap(p.x(),p.y(),"\x06%s",(*i)->getType().getName().c_str());
+    }
+  }
+}
+
+void ExampleAIModule::drawVisibilityData()
+{
+  for(int x=0;x<Broodwar->mapWidth();x++)
+  {
+    for(int y=0;y<Broodwar->mapHeight();y++)
+    {
+      if (Broodwar->isExplored(x,y))
+      {
+        if (Broodwar->isVisible(x,y))
+          Broodwar->drawDotMap(x*32+16,y*32+16,Colors::Green);
+        else
+          Broodwar->drawDotMap(x*32+16,y*32+16,Colors::Blue);
+      }
+      else
+        Broodwar->drawDotMap(x*32+16,y*32+16,Colors::Red);
+    }
+  }
+}
+/*
+void ExampleAIModule::drawTerrainData()
+{
+  //we will iterate through all the base locations, and draw their outlines.
+  for(std::set<BWTA::BaseLocation*>::const_iterator i=BWTA::getBaseLocations().begin();i!=BWTA::getBaseLocations().end();i++)
+  {
+    TilePosition p=(*i)->getTilePosition();
+    Position c=(*i)->getPosition();
+
+    //draw outline of center location
+    Broodwar->drawBox(CoordinateType::Map,p.x()*32,p.y()*32,p.x()*32+4*32,p.y()*32+3*32,Colors::Blue,false);
+
+    //draw a circle at each mineral patch
+    for(std::set<BWAPI::Unit*>::const_iterator j=(*i)->getStaticMinerals().begin();j!=(*i)->getStaticMinerals().end();j++)
+    {
+      Position q=(*j)->getInitialPosition();
+      Broodwar->drawCircle(CoordinateType::Map,q.x(),q.y(),30,Colors::Cyan,false);
+    }
+
+    //draw the outlines of vespene geysers
+    for(std::set<BWAPI::Unit*>::const_iterator j=(*i)->getGeysers().begin();j!=(*i)->getGeysers().end();j++)
+    {
+      TilePosition q=(*j)->getInitialTilePosition();
+      Broodwar->drawBox(CoordinateType::Map,q.x()*32,q.y()*32,q.x()*32+4*32,q.y()*32+2*32,Colors::Orange,false);
+    }
+
+    //if this is an island expansion, draw a yellow circle around the base location
+    if ((*i)->isIsland())
+      Broodwar->drawCircle(CoordinateType::Map,c.x(),c.y(),80,Colors::Yellow,false);
+  }
+
+  //we will iterate through all the regions and draw the polygon outline of it in green.
+  for(std::set<BWTA::Region*>::const_iterator r=BWTA::getRegions().begin();r!=BWTA::getRegions().end();r++)
+  {
+    BWTA::Polygon p=(*r)->getPolygon();
+    for(int j=0;j<(int)p.size();j++)
+    {
+      Position point1=p[j];
+      Position point2=p[(j+1) % p.size()];
+      Broodwar->drawLine(CoordinateType::Map,point1.x(),point1.y(),point2.x(),point2.y(),Colors::Green);
+    }
+  }
+
+  //we will visualize the chokepoints with red lines
+  for(std::set<BWTA::Region*>::const_iterator r=BWTA::getRegions().begin();r!=BWTA::getRegions().end();r++)
+  {
+    for(std::set<BWTA::Chokepoint*>::const_iterator c=(*r)->getChokepoints().begin();c!=(*r)->getChokepoints().end();c++)
+    {
+      Position point1=(*c)->getSides().first;
+      Position point2=(*c)->getSides().second;
+      Broodwar->drawLine(CoordinateType::Map,point1.x(),point1.y(),point2.x(),point2.y(),Colors::Red);
+    }
+  }
+}
+*/
 void ExampleAIModule::showPlayers()
 {
   std::set<Player*> players=Broodwar->getPlayers();
@@ -189,6 +390,7 @@ void ExampleAIModule::showPlayers()
     Broodwar->printf("Player [%d]: %s is in force: %s",(*i)->getID(),(*i)->getName().c_str(), (*i)->getForce()->getName().c_str());
   }
 }
+
 void ExampleAIModule::showForces()
 {
   std::set<Force*> forces=Broodwar->getForces();
@@ -201,12 +403,4 @@ void ExampleAIModule::showForces()
       Broodwar->printf("  - Player [%d]: %s",(*j)->getID(),(*j)->getName().c_str());
     }
   }
-}
-void ExampleAIModule::onReceiveText(BWAPI::Player* player, std::string text)
-{
-  Broodwar->sendText("%s said \"%s\"", player->getName().c_str(), text.c_str());
-}
-void ExampleAIModule::onSaveGame(std::string gameName)
-{
-  Broodwar->printf("The game was saved to \"%s\".", gameName.c_str());
 }
