c61147a73c5e03d83dd951aac6fd9542f42d6d9a
lowerlogic <lowerlogic@gmail.com>
2010-07-06 04:17:04 +0000

    The Test AI Module now runs all the test cases on the Zerg test map FROM THE CLIENT with only 3 failed asserts. :D This is a major step forward toward completing the client-server architecture.
    Haven't ran the client against the other test maps yet.
    
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2524 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/TestAIModule/Source/ZergTest.cpp
+++ bwapi/TestAIModule/Source/ZergTest.cpp
@@ -19,7 +19,6 @@ void ZergTest::onStart()
   Broodwar->sendText("show me the money");
   Broodwar->setLocalSpeed(0);
   this->addTestCase(new CancelMorphTest(UnitTypes::Zerg_Drone));
-  return;
   this->addTestCase(new MorphTest(UnitTypes::Zerg_Drone));
   this->addTestCase(new BuildTest(UnitTypes::Zerg_Hatchery));
   this->addTestCase(new MorphTest(UnitTypes::Zerg_Drone));
