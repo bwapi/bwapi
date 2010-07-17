cd50d66bcca6b9204ca8938ab1705e62780daeca
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-07-17 12:02:08 +0000

    CancelConstructionTest now also tests cancelAddon.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2553 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/UnitImpl.cpp
+++ bwapi/BWAPI/Source/BWAPI/UnitImpl.cpp
@@ -957,10 +957,7 @@ namespace BWAPI
     BW::UnitType type((u16)type1.getID());
     this->orderSelect();
     if (!type.isAddon())
-    {
-      BroodwarImpl.IssueCommand((PBYTE)&BW::Orders::Attack(BW::Position((u16)target.x()*32+type1.tileWidth()*16, (u16)target.y()*32+type1.tileHeight()*16), BW::OrderID::Move), sizeof(BW::Orders::Attack));
       BroodwarImpl.IssueCommand((PBYTE)&BW::Orders::MakeBuilding(BW::TilePosition((u16)target.x(), (u16)target.y()), type), sizeof(BW::Orders::MakeBuilding));
-    }
     else
       BroodwarImpl.IssueCommand((PBYTE)&BW::Orders::MakeAddon(BW::TilePosition((u16)target.x(), (u16)target.y()), type), sizeof(BW::Orders::MakeAddon));
     BroodwarImpl.addToCommandBuffer(new Command(UnitCommand::build(this,target,type1)));
