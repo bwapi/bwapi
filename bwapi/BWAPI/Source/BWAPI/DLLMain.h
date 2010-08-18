dbe908a5ac52a23348369200471feea8e405f5b8
Adam Heinermann <aheinerm@gmail.com>
2010-08-18 21:43:35 +0000

    Replace onIssueCommand with CommandFilter. Reinterpret Starcraft's QueueGameCommand function (previously known as IssueCommand) and implement it in BWAPI.
    This eliminates several asm blocks at the cost of a few Offsets.
    However, the new function uses a standard calling convention, making it easier to use and modify if necessary.
    
    The filter is slightly more lenient by allowing cheat codes, and selecting units via hotkey.
    
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2683 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/DLLMain.h
+++ bwapi/BWAPI/Source/BWAPI/DLLMain.h
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include "BW/Offsets.h"
 
-void NewIssueCommand();
+void __fastcall QueueGameCommand(BYTE *buffer, DWORD length);
 void drawDot(int _x, int _y, int color, int ctype);
 void drawBox(int _x, int _y, int _w, int _h, int color, int ctype);
 void drawText(int _x, int _y, const char* ptext, int ctype, char size);
