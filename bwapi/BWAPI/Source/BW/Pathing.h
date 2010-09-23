92f09192b1607e379316e36ef2445e931dfe4d00
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-09-23 04:56:30 +0000

    More Pathing/Contour research.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2715 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BW/Pathing.h
+++ bwapi/BWAPI/Source/BW/Pathing.h
@@ -28,27 +28,6 @@ namespace BW
     u16 bottom;
   };
 
-  struct unkPath4
-  {
-    u16 unk_00;
-    u16 unk_02;
-    u16 unk_04;
-    u8  unk_06;
-    u8  unk_07;
-    u32 unk_08;
-    u32 unk_0C;
-    u32 x;
-    u32 y;
-  };
-
-  struct unkPath3
-  {
-    u8       unk_00;
-    u8       unk_01;
-    u16      unk_02;
-    unkPath4 *unk_04;
-  };
-
   struct region
   {
     u16       accessabilityFlags;
@@ -57,45 +36,59 @@ namespace BW
               // 0x1FFD = Inaccessable   1101
     u16       groupIndex; // Identifies connected regions
     u16       tileCount;
-    u8        unk_6;
+    u8        pathCount;
     u8        neighborCount;
-    unkPath3  *unk_8;
-    WORD      *neighborsLg; // allocated array of IDs for neighbors
+    u32       unk_8;
+    WORD      *neighbors; // allocated array of IDs for neighbors
     u32       rgnCenterX; // must >> 8; in pixels
     u32       rgnCenterY; // must >> 8; in pixels
     pathRect  rgnBox; // in pixels
     u32       unk_20;
     u32       unk_24;
     u32       unk_28;
-    u16       neighborsSm[10]; // local array of IDs for neighbors
+    u16       localBuffer[10]; // local array of IDs for neighbors
   };
 
   // Contour IDs and values:
-  // 0: BOTTOM: y1, x1, x2, ?
-  // 1: LEFT;   x1, y1, y2, ?
-  // 2: TOP;    y1, x1, x2, ?
-  // 3: RIGHT:  x1, y1, y2, ?
+  // 0: BOTTOM: y1, x1, x2
+  // 1: LEFT;   x1, y1, y2
+  // 2: TOP;    y1, x1, x2
+  // 3: RIGHT:  x1, y1, y2
 
   struct contour
   {
-    pathBox *pathList[4];
-    u16     pathCount[4];
-    u16     pathMax[4];
-    u16     unk_20[4];
-    u16     unk_28[4];
-    u16     unk_30[4];
+    s16 v[3];
+    u8  type;
+    u8  unk_07;
+  };
+
+  struct contourHub
+  {
+    contour *contours[4];
+    u16     contourCount[4];
+    u16     contourMax[4];
+    s16     unk_20[4];
+    s16     unk_28[4];
+    s16     unk_30[4];
+  };
+
+  struct unkCont
+  {
+    u16 unk_00;
+    u16 unk_02;
+    u16 unk_04;
   };
 
   struct SAI_Paths
   {
-    u32       regionCount;
-    u32       unk_92BFC_offset;
-    u32       unk_2000C_offset;
-    u16       mapTileRegionId[256][256];
-    u8        unk_2000C[150000];
-    region    regions[5000];
-    u8        unk_92BFC[20000];
-    contour   *contours; // 0x97A1C
+    u32        regionCount;
+    u32        unk_92BFC_offset;
+    u32        unk_2000C_offset;
+    u16        mapTileRegionId[256][256];
+    unkCont    unk_2000C[25000];  // 0x2000C; need to figure this one out
+    region     regions[5000];     // 0x449FC
+    u16        globalBuffer[10000];  // 0x92BFC; extra buffer used for large neighbor ID arrays
+    contourHub *contoursMain;     // 0x97A1C
   };
 }
 
