69af6c414a111ce9e8fecb3a801cfd1aa6d32ee2
lowerlogic <lowerlogic@gmail.com>
2010-10-19 01:14:34 +0000

    updated Release_Binary with source code from latest release
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2854 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ Release_Binary/ExampleAIModule/Source/ExampleAIModule.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <BWAPI.h>
+
+#include <BWTA.h>
+#include <windows.h>
+
+extern bool analyzed;
+extern bool analysis_just_finished;
+extern BWTA::Region* home;
+extern BWTA::Region* enemy_base;
+DWORD WINAPI AnalyzeThread();
+
+class ExampleAIModule : public BWAPI::AIModule
+{
+public:
+  virtual void onStart();
+  virtual void onEnd(bool isWinner);
+  virtual void onFrame();
+  virtual void onSendText(std::string text);
+  virtual void onReceiveText(BWAPI::Player* player, std::string text);
+  virtual void onPlayerLeft(BWAPI::Player* player);
+  virtual void onNukeDetect(BWAPI::Position target);
+  virtual void onUnitDiscover(BWAPI::Unit* unit);
+  virtual void onUnitEvade(BWAPI::Unit* unit);
+  virtual void onUnitShow(BWAPI::Unit* unit);
+  virtual void onUnitHide(BWAPI::Unit* unit);
+  virtual void onUnitCreate(BWAPI::Unit* unit);
+  virtual void onUnitDestroy(BWAPI::Unit* unit);
+  virtual void onUnitMorph(BWAPI::Unit* unit);
+  virtual void onUnitRenegade(BWAPI::Unit* unit);
+  virtual void onSaveGame(std::string gameName);
+  void drawStats(); //not part of BWAPI::AIModule
+  void drawBullets();
+  void drawVisibilityData();
+  void drawTerrainData();
+  void showPlayers();
+  void showForces();
+  bool show_bullets;
+  bool show_visibility_data;
+};
