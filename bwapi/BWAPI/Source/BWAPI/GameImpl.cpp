891b28092a92c8ebdd7ec5280694da5c944bc242
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-10-23 01:05:34 +0000

    minor changes to canBuildHere, no functionality changes
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@2952 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
+++ bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
@@ -539,7 +539,7 @@ namespace BWAPI
   //--------------------------------------------- CAN BUILD HERE ---------------------------------------------
   bool GameImpl::canBuildHere(Unit* builder, TilePosition position, UnitType type, bool checkExplored)
   {
-    this->setLastError(Errors::Unbuildable_Location);
+    Broodwar->setLastError(Errors::Unbuildable_Location);
     int width  = type.tileWidth();
     int height = type.tileHeight();
 
@@ -553,22 +553,22 @@ namespace BWAPI
       return false;
     if (top < 0) // top
       return false;
-    if (right > this->mapWidth()) // right
+    if (right > Broodwar->mapWidth()) // right
       return false;
-    if (bottom >= this->mapHeight()) // bottom
+    if (bottom >= Broodwar->mapHeight()) // bottom
       return false;
 
     //if the unit is a refinery, we just need to check the set of geysers to see if the position
     //matches one of them (and the type is still vespene geyser)
     if (type.isRefinery())
     {
-      foreach (Unit* g, geysers)
+      foreach (Unit* g, Broodwar->getGeysers())
       {
         if (g->getTilePosition() == position)
         {
           if (g->isVisible() && g->getType() != UnitTypes::Resource_Vespene_Geyser)
             return false;
-          return this->setLastError(Errors::None);
+          return Broodwar->setLastError(Errors::None);
         }
       }
       return false;
@@ -580,7 +580,7 @@ namespace BWAPI
       for(int iy = top; iy < bottom; ++iy)
       {
         // Check if tile is buildable and explored
-        if ( !this->isBuildable(ix, iy) || ( checkExplored && !this->map.isExplored(ix, iy)) )
+        if ( !Broodwar->isBuildable(ix, iy) || ( checkExplored && !Broodwar->isExplored(ix, iy)) )
           return false; // @TODO: Error code for !isExplored ??
 
         // Check if builder is capable of reaching the building site
@@ -596,7 +596,7 @@ namespace BWAPI
     {
       for(int iy = top; iy < bottom; ++iy)
       {
-        foreach(Unit *u, unitsOnTile(ix,iy))
+        foreach(Unit *u, Broodwar->unitsOnTile(ix,iy))
         {
           if ( u != builder &&
                u->getPosition().x() + u->getType().dimensionRight() >= targetX - type.dimensionLeft()  &&
@@ -615,7 +615,7 @@ namespace BWAPI
       if ( !type.isResourceDepot() )
         for(int ix = left; ix < right; ++ix)
           for(int iy = top; iy < bottom; ++iy)
-            if (!BWAPI::Broodwar->hasCreep(ix, iy))
+            if (!Broodwar->hasCreep(ix, iy))
               return false;
     }
     else
@@ -623,18 +623,18 @@ namespace BWAPI
       //Non-zerg buildings cannot be built on creep
       for(int ix = left; ix < right; ++ix)
         for(int iy = top; iy < bottom; ++iy)
-          if (BWAPI::Broodwar->hasCreep(ix, iy))
+          if (Broodwar->hasCreep(ix, iy))
             return false;
     }
 
     /* Power Check */
-    if ( type.requiresPsi() && !this->hasPower(left, top, width, height) )
+    if ( type.requiresPsi() && !Broodwar->hasPower(left, top, width, height) )
       return false;
 
     /* Resource Check (CC, Nex, Hatch) */
     if (type.isResourceDepot())
     {
-      foreach (BWAPI::Unit* m, getStaticMinerals())
+      foreach (BWAPI::Unit* m, Broodwar->getStaticMinerals())
       {
         if (isVisible(m->getInitialTilePosition()) ||
             isVisible(m->getInitialTilePosition().x() + 1, m->getInitialTilePosition().y()))
@@ -646,7 +646,7 @@ namespace BWAPI
             m->getInitialTilePosition().y() < top  + 6)
           return false;
       }
-      foreach (BWAPI::Unit* g, getStaticGeysers())
+      foreach (BWAPI::Unit* g, Broodwar->getStaticGeysers())
       {
         if (g->getInitialTilePosition().x() > left - 7 &&
             g->getInitialTilePosition().y() > top  - 5 &&
@@ -656,7 +656,7 @@ namespace BWAPI
       }
     }
     //if the build site passes all these tests, return true.
-    return this->setLastError(Errors::None);
+    return Broodwar->setLastError(Errors::None);
   }
   //--------------------------------------------- CAN MAKE ---------------------------------------------------
   bool  GameImpl::canMake(Unit* builder, UnitType type)
