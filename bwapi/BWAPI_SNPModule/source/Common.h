804566d9ec9db9d91be080f7d01e3a6a3ec4ba47
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-11-06 18:28:51 +0000

    SNP: Better error description, a macro, and change port 6111 to 6112
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3034 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI_SNPModule/source/Common.h
+++ bwapi/BWAPI_SNPModule/source/Common.h
@@ -10,6 +10,8 @@ extern char gszLogPath[MAX_PATH];
 #define w(x) MessageBox(NULL, x, "!", MB_OK | MB_ICONWARNING)
 #define e(x) MessageBox(NULL, x, "Error!", MB_OK | MB_ICONERROR)
 
+#define ip(x) ( inet_ntoa(*(in_addr*)&x[2]) )
+
 void Error(DWORD dwErrCode, const char *format = "", ...);
 void Log(const char *format = "", ...);
 void LogBytes(char *pBuffer, DWORD dwSize, const char *format, ...);
