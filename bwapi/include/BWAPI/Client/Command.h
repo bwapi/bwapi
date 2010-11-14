52f587d5b87a8a53a4fe1f7bccefa20cfe1cb356
lowerlogic <lowerlogic@gmail.com>
2010-11-14 22:49:29 +0000

    Fix for issue 282.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3102 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/include/BWAPI/Client/Command.h
+++ bwapi/include/BWAPI/Client/Command.h
@@ -5,11 +5,15 @@ namespace BWAPIC
 {
   struct Command
   {
+    Command()
+    {
+      type = CommandType::None;
+    }
     Command(CommandType::Enum _commandType, int _value1=0, int _value2=0)
     {
-      type=_commandType;
-      value1=_value1;
-      value2=_value2;
+      type   = _commandType;
+      value1 = _value1;
+      value2 = _value2;
     }
     CommandType::Enum type;
     int value1;
