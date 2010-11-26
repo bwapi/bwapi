8b8b2ade69bf02ed8e52910c77c3a161e92d658b
Adam Heinermann <aheinerm@gmail.com>
2010-11-26 02:57:05 +0000

    Ignore all Release/Debug folders and user files. Completely forgot to include DevAIModule wiki files.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3218 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/DevAIModule/Source/Wiki.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include "DevAIModule.h"
+
+#define makelink(x) wiki(x.getName()).c_str(), x.getName().c_str()
+#define maketitle(x) wiki(x.getName(), true).c_str()
+
+void writeUnitWiki();
