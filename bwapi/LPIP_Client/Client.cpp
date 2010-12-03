b9ec97b3859fbd5b88f6d307841e94c36c4fbf7f
lowerlogic <lowerlogic@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-12-03 01:56:28 +0000

    shared memory works!
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3268 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/LPIP_Client/Client.cpp
+++ bwapi/LPIP_Client/Client.cpp
@@ -6,53 +6,26 @@
 #include <stdio.h>
 #include <time.h>
 using namespace std;
-struct GameInfo
-{
-	int szSize;
-	int flType;
-	DWORD dwGameState;
-	char chGameName[128];
-	char chGameStats[128];
-};
-struct GameInfoTable
-{
-  GameInfo gameInfo[256];
-  time_t gameInfoLastUpdate[256];
-};
+#include "../LPIP_Server/SharedMemory.h"
 int main(int argc, char* argv[])
 {
   cout << "Starting Client...\n";
-
-  HANDLE mapFileHandle = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, "Global\\bwapi_local_lpip_games1234");
-  GameInfoTable* data = NULL;
-  if (mapFileHandle == INVALID_HANDLE_VALUE)
+  SharedMemory s;
+  while (!s.connect())
   {
-    printf("Shared memory does not exist, creating...\n");
-    //cannot connect to shared memory, so try to create it
-    int size = sizeof(GameInfoTable);
-    mapFileHandle = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, size, "Global\\bwapi_local_lpip_games1234");
-    if (mapFileHandle == INVALID_HANDLE_VALUE)
-    {
-      printf("Error: unable to make shared memory\n");
-      return false;
-    }
-    data = (GameInfoTable*) MapViewOfFile(mapFileHandle, FILE_MAP_ALL_ACCESS,0,0,sizeof(GameInfoTable));
-    for(int i=0;i<256;i++)
-      data->gameInfoLastUpdate[i] = 0;
   }
-  else
-    data = (GameInfoTable*) MapViewOfFile(mapFileHandle, FILE_MAP_ALL_ACCESS,0,0,sizeof(GameInfoTable));
 
   printf("Checking shared memory for games...\n");
   time_t now = time(NULL);
   for(int i=0;i<256;i++)
   {
-    if (data->gameInfoLastUpdate[i]<=now && (now-data->gameInfoLastUpdate[i])<(time_t)(3*60))
+    if (s.data->gameInfoLastUpdate[i]<=now && (now-s.data->gameInfoLastUpdate[i])<(time_t)(3*60))
     {
-      data->gameInfo[i].chGameName[127]='\0';
-      printf("Game[%i] Name=%s",i,data->gameInfo[i].chGameName);
+      s.data->gameInfo[i].chGameName[127]='\0';
+      printf("Game[%i] Name=%s\n",i,s.data->gameInfo[i].chGameName);
     }
   }
   system("pause");
+  s.disconnect();
 	return 0;
 }
