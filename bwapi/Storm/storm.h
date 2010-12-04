cf60935f7ed826c22739ca4f30a3ee0d976a06a7
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-12-04 18:31:40 +0000

    Alter LPIP network mode settings.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3287 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/Storm/storm.h
+++ bwapi/Storm/storm.h
@@ -76,15 +76,15 @@ BOOL STORMAPI SNetGetNumPlayers(int *firstplayerid, int *lastplayerid, int *acti
 #define CAPS_STRUCT
 struct caps
 {
-  DWORD dwSize;
-  DWORD dwUnk1;
-  DWORD dwBufferSize;
-  DWORD dwUnk3;
-  DWORD dwUnk4;
-  DWORD dwUnk5;
-  DWORD dwPingTimeout;
-  DWORD dwPlayerCount;
-  DWORD dwLatencyCalls;
+  DWORD dwSize;       // Size of this structure  // sizeof(caps)
+  DWORD dwUnk_0x04;
+  DWORD dwBufferSize; // Size of the packet buffer, must be beteen 128 and 512
+  DWORD dwUnk_0x0C;
+  DWORD dwUnk_0x10;
+  DWORD dwUnk_0x14;   // some kind of timeout or timer related
+  DWORD dwPlayerLatency;  // ... latency?
+  DWORD dwPlayerCount;  // the number of players that can participate, must be between 1 and 20
+  DWORD dwCallDelay;  // the number of calls before data is sent over the network // between 2 and 8; single player is set to 1
 };
 #endif
 BOOL STORMAPI SNetGetPlayerCaps(char playerid, caps *playerCaps);
