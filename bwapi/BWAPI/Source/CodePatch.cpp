e66fcc9a88045f9ee5349a52085abab1d0b9aab6
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2010-12-11 17:18:39 +0000

    Move stuff from DLLMain to Hacks/CodePatch and Hacks/Detours. Move drawing-related functions from GameImpl.cpp to GameDrawing.cpp.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3341 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/BWAPI/Source/CodePatch.cpp
@@ -0,0 +1,73 @@
+#include "CodePatch.h"
+#include "NewHackUtil.h"
+
+#include "BW/Offsets.h"
+#include "BWAPI/GameImpl.h"
+#include "Detours.h"
+#include "Resolution.h"
+#include "WMode.h"
+
+void ApplyCodePatches()
+{
+  /* Create function-level hooks */
+  HackUtil::CallPatch(BW::BWFXN_NextLogicFrame, &_nextFrameHook);
+  HackUtil::JmpPatch(BW::BWFXN_QueueCommand,    &CommandFilter);
+  HackUtil::JmpPatch(HackUtil::GetImport("storm.dll", 251), &_SFileAuthenticateArchive);
+  HackUtil::JmpPatch(BW::BWFXN_DDrawDestroy,    &DDrawDestroy);
+  HackUtil::JmpPatch(BW::BWFXN_NetSelectReturnMenu, &_SelectReturnMenu);
+
+  /* Perform code patches */
+  char zero = 0;
+  HackUtil::WriteNops(BW::BWDATA_MenuLoadHack, 11);            // main menu load timer
+  HackUtil::WriteMem(BW::BWDATA_MenuInHack, &zero, 1);         // menu in speed
+  HackUtil::WriteMem(BW::BWDATA_MenuOutHack, &zero, 1);        // menu out speed
+  HackUtil::WriteMem(BW::BWDATA_MultiplayerHack, &zero, 1);    // BNET Server menu in speed
+  HackUtil::WriteMem(BW::BWDATA_MultiplayerHack2, &zero, 1);   // BNET Server menu out speed
+  HackUtil::WriteMem(BW::BWDATA_OpponentStartHack, &zero, 1);  // Start without an opponent
+
+  /* Create import detours */
+  HackUtil::PatchImport("storm.dll", 119, &_SNetLeaveGame);
+  HackUtil::PatchImport("storm.dll", 121, &_SNetReceiveMessage);
+  HackUtil::PatchImport("storm.dll", 128, &_SNetSendTurn);
+  HackUtil::PatchImport("storm.dll", 268, &_SFileOpenFileEx);
+  HackUtil::PatchImport("storm.dll", 401, &_SMemAlloc);
+  HackUtil::PatchImport("storm.dll", 501, &_SStrCopy);
+
+  // wmode/drawing
+  HackUtil::PatchImport("user32.dll", "GetCursorPos", &_GetCursorPos);
+  HackUtil::PatchImport("user32.dll", "SetCursorPos", &_SetCursorPos);
+  HackUtil::PatchImport("user32.dll", "ClipCursor", &_ClipCursor);
+  HackUtil::PatchImport("storm.dll", 350, &_SDrawLockSurface);
+  HackUtil::PatchImport("storm.dll", 354, &_SDrawRealizePalette);
+  HackUtil::PatchImport("storm.dll", 356, &_SDrawUnlockSurface);
+  HackUtil::PatchImport("storm.dll", 357, &_SDrawUpdatePalette);
+}
+
+//----------------------------------------- NET-MODE RETURN MENU ---------------------------------------------
+void _SelectReturnMenu()
+{
+  switch ( *BW::BWDATA_NetMode )
+  {
+  case 'BNET':
+    *BW::BWDATA_glGluesMode = 4;  // battle.net
+    break;
+  case 'IPXN':
+  case 'ATLK':
+  case 'IPXX':
+  case 'UDPN':
+  case 'LUDP':
+  case 'LPIP':
+    *BW::BWDATA_glGluesMode = 10; // game select
+    break;
+  case 'MDMX':
+  case 'MODM':
+    *BW::BWDATA_glGluesMode = 20; // modem
+    break;
+  case 'SCBL':
+    *BW::BWDATA_glGluesMode = 21; // direct connect
+    break;
+  default:
+    *BW::BWDATA_glGluesMode = 0;  // main menu
+    break;
+  }
+}
---
+++ bwapi/BWAPI/Source/Detours.cpp
@@ -0,0 +1,270 @@
+#include <windows.h>
+#include <string>
+#include "../Storm/storm.h"
+
+#include "WMode.h"
+//#include "Resolution.h"
+#include "Holiday/Holiday.h"
+
+#include "Detours.h"
+#include "BWAPI/GameImpl.h"
+#include "BW/Offsets.h"
+#include "DLLMain.h"
+
+bool hideHUD;
+
+//----------------------------------------------- ON GAME END ------------------------------------------------
+BOOL __stdcall _SNetLeaveGame(int type)
+{
+  //MessageBox(0, "OnGameEnd", "", 0);
+  BWAPI::BroodwarImpl.onGameEnd();
+  return SNetLeaveGame(type);
+}
+
+//--------------------------------------------- NEXT FRAME HOOK ----------------------------------------------
+int __cdecl _nextFrameHook()
+{
+  BWAPI::BroodwarImpl.update();
+  return *BW::BWDATA_NextLogicFrameData;
+}
+
+//------------------------------------------------- SEND TEXT ------------------------------------------------
+int __stdcall _SStrCopy(char *dest, const char *source, size_t size)
+{
+  if ( strlen(source) > 0 )
+  {
+    if ( size == 0x7FFFFFFF && *BW::BWDATA_gwGameMode == 3 )
+    {
+      if ( dest == BW::BWDATA_SaveGameFile )
+      {
+        /* onSaveGame */
+        BWAPI::BroodwarImpl.onSaveGame((char*)source);
+      }
+      else
+      {
+        /* onSend Game */
+        BWAPI::BroodwarImpl.sentMessages.push_back(std::string(source));
+        dest[0] = 0;
+        return 0;
+      }
+    }
+    else if ( size == 120 )
+    {
+      /* onSend Lobby */
+    }
+  }
+  return SStrCopy(dest, source, size);
+}
+
+//----------------------------------------------- RECEIVE TEXT -----------------------------------------------
+BOOL __stdcall _SNetReceiveMessage(int *senderplayerid, u8 **data, int *databytes)
+{
+  BOOL rval = SNetReceiveMessage(senderplayerid, (char**)data, databytes);
+  if ( rval && *databytes > 2 && (*data)[0] == 0)
+    BWAPI::BroodwarImpl.onReceiveText(*senderplayerid, std::string((char*)&(*data)[2]) );
+
+  return rval;
+}
+
+//----------------------------------------------- DRAW HOOK --------------------------------------------------
+bool wantRefresh = false;
+void __stdcall DrawHook(BW::bitmap *pSurface, BW::bounds *pBounds)
+{
+  if ( wantRefresh )
+  {
+    wantRefresh = false;
+    memset(BW::BWDATA_RefreshRegions, 1, 1200);
+  }
+
+  //GameUpdate(pSurface, pBounds);
+  if ( BW::pOldDrawGameProc )
+    BW::pOldDrawGameProc(pSurface, pBounds);
+
+  if ( BW::BWDATA_GameScreenBuffer->data )
+  {
+    if ( gdwHoliday )
+      DrawHoliday();
+
+    unsigned int numShapes = BWAPI::BroodwarImpl.shapes.size();
+    for( unsigned int i = 0; i < numShapes; ++i )
+      BWAPI::BroodwarImpl.shapes[i]->draw();
+    
+    if ( numShapes )
+      wantRefresh = true;
+  }
+}
+//------------------------------------------------- MENU HOOK ------------------------------------------------
+bool nosound = false;
+void __stdcall DrawDialogHook(BW::bitmap *pSurface, BW::bounds *pBounds)
+{
+  if ( BW::pOldDrawDialogProc && !hideHUD )
+    BW::pOldDrawDialogProc(pSurface, pBounds);
+
+  if ( *BW::BWDATA_gwGameMode == 4 )
+    BWAPI::BroodwarImpl.onMenuFrame();
+
+  if ( !nosound )
+  {
+    nosound = true;
+    char szNosound[8];
+    GetPrivateProfileString("starcraft", "sound", "ON", szNosound, 8, szConfigPath);
+    if ( strcmpi(szNosound, "OFF") == 0 )
+      BW::BWDATA_DSoundDestroy();
+  }
+
+  //click the menu dialog that pops up when you win/lose a game
+  BW::dialog *endDialog = BW::FindDialogGlobal("LMission");
+  if ( !endDialog )
+    endDialog = BW::FindDialogGlobal("WMission");
+  if ( endDialog )
+    endDialog->findIndex(-2)->activate();
+
+  if ( wmode && ghMainWnd )
+    InvalidateRect(ghMainWnd, NULL, FALSE);
+}
+
+//------------------------------------------- AUTH ARCHIVE HOOK ----------------------------------------------
+BOOL __stdcall _SFileAuthenticateArchive(HANDLE hArchive, DWORD *dwReturnVal)
+{
+  /* Always return a successful check to bypass our custom SNP module authentication */
+  if ( dwReturnVal )
+    *dwReturnVal = 5;
+  return TRUE;
+}
+
+//--------------------------------------------- OPEN FILE HOOK -----------------------------------------------
+std::string lastFile;
+BOOL __stdcall _SFileOpenFileEx(HANDLE hMpq, const char *szFileName, DWORD dwSearchScope, HANDLE *phFile)
+{
+  /* Store the name of the last-opened file to retrieve the pointer once it's allocated */
+  lastFile = szFileName;
+  return SFileOpenFileEx(hMpq, szFileName, dwSearchScope, phFile);
+}
+
+//--------------------------------------------- MEM ALLOC HOOK -----------------------------------------------
+void *__stdcall _SMemAlloc(int amount, char *logfilename, int logline, char defaultValue)
+{
+  /* Call the original function */
+  void *rval = SMemAlloc(amount, logfilename, logline, defaultValue);
+
+  /* Save the allocated string table pointer */
+  if ( lastFile == "rez\\stat_txt.tbl" )
+  {
+    BW::BWDATA_StringTableOff = (char*)rval;
+    lastFile = "";
+    //MessageBox(0, "BWDATA_StringTableOff", "", 0);
+  }
+
+  /* Save the allocated fog of war pointer */
+  if ( amount == 0x40000 && strcmpi(logfilename, "Starcraft\\SWAR\\lang\\Gamemap.cpp") == 0 && logline == 606 )
+  {
+    BW::BWDATA_MapFogOfWar = (u32*)rval;
+    //MessageBox(0, "BWDATA_MapFogOfWar", "", 0);
+  }
+
+  /* Save the allocated mini-tile flags pointer */
+  if ( lastFile.find(".vf4") != std::string::npos )
+  {
+    BW::BWDATA_MiniTileFlags = (BW::MiniTileMaps_type*)rval;
+    lastFile = "";
+    //MessageBox(0, "BWDATA_MiniTileFlags", "", 0);
+  }
+
+  /* Save the allocated creep pointer */
+  if ( strcmpi(logfilename, "Starcraft\\SWAR\\MapComn\\creep.cpp") == 0 && logline == 420 )
+  {
+    BW::BWDATA_ZergCreepArray = (u16*)rval;
+    //MessageBox(0, "BWDATA_ZergCreepArray", "", 0);
+  }
+
+  /* Save the allocated SAI_Paths pointer */
+  if ( strcmpi(logfilename, "Starcraft\\SWAR\\lang\\sai_PathCreate.cpp") == 0 && logline == 210 )
+  {
+    BW::BWDATA_SAIPathing = (BW::SAI_Paths*)rval;
+    //MessageBox(0, "BWDATA_SAIPathing", "", 0);
+  }
+
+  /* Save the allocated tileset pointer */
+  if ( lastFile.find(".cv5") != std::string::npos )
+  {
+    BW::BWDATA_TileSet    = (BW::TileType*)rval;
+    lastFile = "";
+    //MessageBox(0, "BWDATA_TileSet", "", 0);
+  }
+
+  /* Save the allocated map tile array pointer */
+  if ( amount == 0x20000 && strcmpi(logfilename, "Starcraft\\SWAR\\lang\\Gamemap.cpp") == 0 && logline == 603 )
+  {
+    BW::BWDATA_MapTileArray = (u16*)rval;
+    //MessageBox(0, "BWDATA_MapTileArray", "", 0);
+  }
+
+  return rval;
+}
+
+//--------------------------------------------- SEND TURN HOOK -----------------------------------------------
+DWORD lastTurnTime;
+DWORD lastTurnFrame;
+BOOL __stdcall _SNetSendTurn(char *data, unsigned int databytes)
+{
+  /* Save tick/frame counts for getRemainingLatency*  */
+  lastTurnTime  = GetTickCount();
+  lastTurnFrame = BWAPI::BroodwarImpl.getFrameCount();
+  return SNetSendTurn(data, databytes);
+}
+
+//---------------------------------------- USER ISSUE COMMAND HOOK -------------------------------------------
+int   lastHotkey;
+DWORD lastHotkeyTime;
+void __fastcall CommandFilter(BYTE *buffer, DWORD length)
+{
+  /* Filter commands using BWAPI rules */
+  if ( BWAPI::BroodwarImpl.isFlagEnabled(BWAPI::Flag::UserInput) || 
+       !BWAPI::BroodwarImpl.onStartCalled ||
+       buffer[0] <= 0x0B ||
+       (buffer[0] >= 0x0F && buffer[0] <= 0x12) ||
+       (length >= 3 && buffer[0] == 0x13 && buffer[1] == 1)    || // Hotkey (select only)
+       (buffer[0] >= 0x37 && buffer[0] <= 0x59) ||
+       buffer[0] >= 0x5B )
+  {
+    // Custom select code
+    if ( buffer[0] == 0x09 ||
+         buffer[0] == 0x0A ||
+         buffer[0] == 0x0B ||
+         (length >= 3 && buffer[0] == 0x13 && buffer[1] == 1) ) // Select Units
+    {
+      // Do our own center view on hotkeys, since BWAPI introduces a bug that destroys this
+      if ( length >= 3 && buffer[0] == 0x13 && buffer[1] == 1 ) // Recall Hotkey
+      {
+        DWORD thisHotkeyTime = GetTickCount();
+        if ( lastHotkey == buffer[2] && (thisHotkeyTime - lastHotkeyTime) < 800 )
+        {
+          // do center view here
+          BWAPI::BroodwarImpl.moveToSelected();
+          lastHotkeyTime = 0;
+          lastHotkey     = -1;
+        }
+        else
+        {
+          lastHotkeyTime = thisHotkeyTime;
+          lastHotkey     = buffer[2];
+        }
+      }
+      BWAPI::BroodwarImpl.wantSelectionUpdate = true;
+      return;
+    } // selections
+
+    if ( buffer[0] == 0x0C ||
+         (buffer[0] == 0x13 && !(buffer[1] & 1)) ||
+         buffer[0] == 0x14 ||
+         buffer[0] == 0x15 ||
+         (buffer[0] >= 0x18 && buffer[0] <= 0x36) ||
+         buffer[0] == 0x5A )
+    {
+      //reload the unit selection states (so that the user doesn't notice any changes in selected units in the Starcraft GUI.
+      BW::Orders::Select sel = BW::Orders::Select(*BW::BWDATA_ClientSelectionCount, BW::BWDATA_ClientSelectionGroup);
+      QueueGameCommand((PBYTE)&sel, sel.size);
+    } // user select
+    QueueGameCommand(buffer, length);
+  }
+}
