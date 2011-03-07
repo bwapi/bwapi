9ad7f87a4f20581e296d0b8cfc1f7db7c4aafa2b
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2011-03-07 21:05:13 +0000

    Remove the SNP project and just use the binary from the BWDI project. For network module updates and support, visit the BWDI ( http://bwdi.googlecode.com ) homepage.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3528 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/SNP_Network/Source/SettingsDialog.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void showSettingsDialog();
-void hideSettingsDialog();
-
-const char* getHostIPString();
-const char* getHostPortString();
-const char* getLocalPortString();
-void setStatusString(const char *statusText);
