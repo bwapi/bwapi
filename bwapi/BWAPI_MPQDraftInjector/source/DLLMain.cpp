b032ff3991357ca136fe82fdfc543546ba6a571c
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2011-04-23 04:59:41 +0000

    Fix issue with loader registry check. More consistent registry and error handling for loaders.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3662 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI_MPQDraftInjector/source/DLLMain.cpp
+++ bwapi/BWAPI_MPQDraftInjector/source/DLLMain.cpp
@@ -1,7 +1,5 @@
 #include "MPQDraftPlugin.h"
 #include <windows.h>
-#include <string>
-#include <assert.h>
 #include <stdio.h>
 
 #include "../../svnrev.h"
@@ -18,6 +16,44 @@
 #endif
 
 const char* plugin_name = "BWAPI Injector (" STARCRAFT_VER ") r" SVN_REV_STR " (" BUILD_STR ")";
+
+DWORD GetRegString(HKEY hBaseKey, const char *pszSubKey, const char *pszValueName, char *pszOutput, DWORD *dwOutSize)
+{
+  HKEY hKey = NULL;;
+  DWORD dwErrCode = RegOpenKeyEx(hBaseKey, pszSubKey, 0, KEY_QUERY_VALUE, &hKey);
+  if ( dwErrCode != ERROR_SUCCESS )
+    return dwErrCode;
+
+  dwErrCode = RegQueryValueEx(hKey, pszValueName, NULL, NULL, (LPBYTE)pszOutput, dwOutSize);
+  RegCloseKey(hKey);
+  return dwErrCode;
+}
+
+BOOL BWAPIError(DWORD dwErrCode, const char *format, ...)
+{
+  char buffer[MAX_PATH];
+  va_list ap;
+  va_start(ap, format);
+  vsnprintf_s(buffer, MAX_PATH, MAX_PATH, format, ap);
+  va_end(ap);
+
+  char szErrMsg[256];
+  FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwErrCode, 0, szErrMsg, MAX_PATH, NULL);
+
+  SYSTEMTIME time;
+  GetSystemTime(&time);
+  FILE* f = fopen("bwapi-error.txt", "a+");
+  if ( f )
+  {
+    fprintf(f, "[%u/%02u/%02u - %02u:%02u:%02u] %s - %s\n", time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute, time.wSecond, buffer, szErrMsg);
+    fclose(f);
+  }
+
+  char buffer2[MAX_PATH*2];
+  sprintf(buffer2, "%s\n%s", buffer, szErrMsg);
+  MessageBox(NULL, buffer2, "Error", MB_OK | MB_ICONERROR);
+  return FALSE;
+}
 
 BOOL BWAPIError(const char *format, ...)
 {
@@ -68,36 +104,20 @@ class MPQDraftPluginInterface : public IMPQDraftPlugin
     {
       char szBwPath[MAX_PATH];
       DWORD dwPathSize = MAX_PATH;
-      HKEY hKey;
 
-      char szErrString[256];
-      DWORD dwErrCode = RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\Blizzard Entertainment\\Starcraft", 0, KEY_QUERY_VALUE, &hKey);
+      DWORD dwErrCode = GetRegString(HKEY_CURRENT_USER, "SOFTWARE\\Blizzard Entertainment\\Starcraft", "InstallPath", szBwPath, &dwPathSize);
       if ( dwErrCode != ERROR_SUCCESS )
       {
-        dwErrCode = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Blizzard Entertainment\\Starcraft", 0, KEY_QUERY_VALUE, &hKey);
+        dwErrCode = GetRegString(HKEY_LOCAL_MACHINE, "SOFTWARE\\Blizzard Entertainment\\Starcraft", "InstallPath", szBwPath, &dwPathSize);
         if ( dwErrCode != ERROR_SUCCESS )
-        {
-          FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwErrCode, 0, szErrString, 256, NULL);
-          return BWAPIError("An error occured when opening the registry key:\n0x%p\n%s", dwErrCode, szErrString);
-        }
+          return BWAPIError(dwErrCode, "An error occured when retrieving the registry key.");
       }
-
-      if ( !hKey )
-        return FALSE;
-
-      dwErrCode = RegQueryValueEx(hKey, "InstallPath", NULL, NULL, (LPBYTE)szBwPath, &dwPathSize);
-      if ( dwErrCode != ERROR_SUCCESS )
-      {
-        FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwErrCode, 0, szErrString, 256, NULL);
-        return BWAPIError("An error occured when querying the registry value:\n0x%p\n%s", dwErrCode, szErrString);
-      }
-      RegCloseKey(hKey);
 
       // Load the config file
       char szExecPath[MAX_PATH*2];
       strcpy(szExecPath, szBwPath);
       strcat(szExecPath, "\\bwapi-data\\bwapi.ini");
-      if ( !ShellExecute(NULL, "open", szExecPath, NULL, NULL, SW_SHOWNORMAL) )
+      if ( ShellExecute(NULL, "open", szExecPath, NULL, NULL, SW_SHOWNORMAL) <= (HINSTANCE)32 )
         return BWAPIError("Unable to open BWAPI config file.");
       return TRUE;
     }
@@ -122,7 +142,7 @@ class MPQDraftPluginInterface : public IMPQDraftPlugin
       {
         envFailed = true;
         if ( !GetCurrentDirectory(MAX_PATH, envBuffer) )
-          return BWAPIError("Could not find ChaosDir or CurrentDirectory. \nError Code: 0x%p", GetLastError());
+          return BWAPIError(GetLastError(), "Could not find ChaosDir or CurrentDirectory.");
       }
 
       strcat(envBuffer, "\\" MODULE);
@@ -130,15 +150,15 @@ class MPQDraftPluginInterface : public IMPQDraftPlugin
       if ( dwFileAttribs == INVALID_FILE_ATTRIBUTES || dwFileAttribs & FILE_ATTRIBUTE_DIRECTORY )
       {
         if ( !envFailed && !GetCurrentDirectory(MAX_PATH, envBuffer) )
-          return BWAPIError("Could not find CurrentDirectory. \nError Code: 0x%p", GetLastError());
+          return BWAPIError(GetLastError(), "Could not find CurrentDirectory.");
         strcat(envBuffer, "\\" MODULE);
         dwFileAttribs = GetFileAttributes(envBuffer);
         if ( dwFileAttribs == INVALID_FILE_ATTRIBUTES || dwFileAttribs & FILE_ATTRIBUTE_DIRECTORY )
-          return BWAPIError("Could not find file \"%s\". \nError Code: 0x%p", envBuffer, GetLastError());
+          return BWAPIError(GetLastError(), "Could not find file \"%s\".", envBuffer);
       }
 
       if ( !LoadLibrary(envBuffer) )
-        return BWAPIError("Could not load \"%s\". \nError Code: 0x%p", envBuffer, GetLastError());
+        return BWAPIError(GetLastError(), "Could not load \"%s\".", envBuffer);
       return TRUE;
     }
     BOOL WINAPI TerminatePlugin()
