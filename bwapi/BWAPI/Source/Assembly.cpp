e5b4a3e61c99e6d646c751ac54bac5f46cadd1d0
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2011-08-06 21:49:49 +0000

    Reduce the size of Assembly.cpp by using assembly macros.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3832 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BWAPI/Source/Assembly.cpp
+++ bwapi/BWAPI/Source/Assembly.cpp
@@ -8,35 +8,35 @@
 
 DWORD d_eaxSave, d_ebxSave, d_ecxSave, d_edxSave, d_esiSave, d_ediSave, d_espSave, d_ebpSave;
 
+#define SAVE_REGISTERS() __asm {  __asm mov d_eaxSave, eax  \
+                                  __asm mov d_ebxSave, ebx  \
+                                  __asm mov d_ecxSave, ecx  \
+                                  __asm mov d_edxSave, edx  \
+                                  __asm mov d_esiSave, esi  \
+                                  __asm mov d_ediSave, edi  \
+                                  __asm mov d_espSave, esp  \
+                                  __asm mov d_ebpSave, ebp }
+
+#define LOAD_REGISTERS() __asm {  __asm mov eax, d_eaxSave  \
+                                  __asm mov ebx, d_ebxSave  \
+                                  __asm mov ecx, d_ecxSave  \
+                                  __asm mov edx, d_edxSave  \
+                                  __asm mov esi, d_esiSave  \
+                                  __asm mov edi, d_ediSave  \
+                                  __asm mov esp, d_espSave  \
+                                  __asm mov ebp, d_ebpSave }
+
 //--------------------------------------------- REPAIR HOOK --------------------------------------------------
 void __declspec(naked) _repairHook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
+
   BWAPI::BroodwarImpl.players[d_eaxSave]->_repairedMinerals += d_ebxSave;
   BWAPI::BroodwarImpl.players[d_eaxSave]->_repairedGas      += d_ediSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     mov ecx, dword ptr ds:[eax*4+0x57f120]
     jmp BW::BWFXN_SpendRepairReturnAddress
   }
@@ -45,31 +45,12 @@ void __declspec(naked) _repairHook()
 //--------------------------------------------- REFUND MINERALS HOOK --------------------------------------------
 void __declspec(naked) _refundMineralsHook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_ecxSave/4]->_refundedMinerals += d_eaxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     movzx eax, word ptr ds:[esi+0x65fd00]
     jmp BW::BWFXN_RefundMineralsReturnAddress
   }
@@ -79,31 +60,12 @@ void __declspec(naked) _refundMineralsHook()
 //--------------------------------------------- REFUND GAS HOOK --------------------------------------------
 void __declspec(naked) _refundGasHook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_ecxSave/4]->_refundedGas += d_eaxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     mov dword ptr ds:[ecx+0x57F120], edx
     jmp BW::BWFXN_RefundGasReturnAddress
   }
@@ -113,32 +75,13 @@ void __declspec(naked) _refundGasHook()
 //--------------------------------------------- REFUND MIN AND GAS 2 HOOK --------------------------------------------
 void __declspec(naked) _refundMinAndGas2Hook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_eaxSave/4]->_refundedMinerals += d_edxSave;
   BWAPI::BroodwarImpl.players[d_eaxSave/4]->_refundedGas += d_ecxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     add dword ptr ds:[eax+0x57F0F0], edx
     jmp BW::BWFXN_RefundMinAndGas2ReturnAddress
   }
@@ -149,31 +92,12 @@ void __declspec(naked) _refundMinAndGas2Hook()
 //--------------------------------------------- REFUND MIN 3 HOOK --------------------------------------------
 void __declspec(naked) _refundMin3Hook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_ecxSave/4]->_refundedMinerals += d_eaxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     movzx eax, word ptr ds:[edi+0x65FD00]
     jmp BW::BWFXN_RefundMin3ReturnAddress
   }
@@ -183,31 +107,12 @@ void __declspec(naked) _refundMin3Hook()
 //--------------------------------------------- REFUND GAS 3 HOOK --------------------------------------------
 void __declspec(naked) _refundGas3Hook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_ecxSave/4]->_refundedGas += d_eaxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     mov dword ptr ds:[ecx+0x57F120], edx
     jmp BW::BWFXN_RefundGas3ReturnAddress
   }
@@ -217,30 +122,12 @@ void __declspec(naked) _refundGas3Hook()
 //--------------------------------------------- REFUND MIN 4 HOOK --------------------------------------------
 void __declspec(naked) _refundMin4Hook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_esiSave]->_refundedMinerals += d_edxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
     cmp cx, 0x2e
     mov dword ptr ds:[esi*4+0x57F0F0], edi
     jmp BW::BWFXN_RefundMin4ReturnAddress
@@ -251,31 +138,12 @@ void __declspec(naked) _refundMin4Hook()
 //--------------------------------------------- REFUND GAS 4 HOOK --------------------------------------------
 void __declspec(naked) _refundGas4Hook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_esiSave]->_refundedGas += d_edxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     lea ecx, dword ptr ds:[esi*4+0x57F120]
     jmp BW::BWFXN_RefundGas4ReturnAddress
   }
@@ -284,32 +152,13 @@ void __declspec(naked) _refundGas4Hook()
 //--------------------------------------------- REFUND MIN AND GAS 5 HOOK --------------------------------------------
 void __declspec(naked) _refundMinAndGas5Hook()
 {
-  
-  __asm
-  {
-    mov d_eaxSave, eax
-    mov d_ebxSave, ebx
-    mov d_ecxSave, ecx
-    mov d_edxSave, edx
-    mov d_esiSave, esi
-    mov d_ediSave, edi
-    mov d_espSave, esp
-    mov d_ebpSave, ebp
-  }
+  SAVE_REGISTERS();
   BWAPI::BroodwarImpl.players[d_eaxSave/4]->_refundedMinerals += d_esiSave;
   BWAPI::BroodwarImpl.players[d_eaxSave/4]->_refundedGas += d_ecxSave;
 
+  LOAD_REGISTERS();
   __asm
   {
-    mov eax, d_eaxSave
-    mov ebx, d_ebxSave
-    mov ecx, d_ecxSave
-    mov edx, d_edxSave
-    mov esi, d_esiSave
-    mov edi, d_ediSave
-    mov esp, d_espSave
-    mov ebp, d_ebpSave
-
     add dword ptr ds:[eax+0x57F120], ecx
     jmp BW::BWFXN_RefundMinAndGas5ReturnAddress
   }
