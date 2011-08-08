9bdbae2692db14d6255feb2852d848ef03107500
deathknight13579@aim.com <deathknight13579@aim.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2011-08-08 17:40:34 +0000

    Added BulletTypes::Ensnare. Use macros to reduce redundancy in many BWAPILIB type definitions.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3837 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/DevAIModule/Source/DevAIModule.cpp
+++ bwapi/DevAIModule/Source/DevAIModule.cpp
@@ -1,9 +1,9 @@
 #include "DevAIModule.h"
 #include <set>
+#include <math.h>
 
 using namespace BWAPI;
 
-bool enabled;
 int mapH, mapW;
 
 DWORD dwCount = 0;
@@ -12,7 +12,6 @@ void DevAIModule::onStart()
 {
   // enable stuff
   bw->enableFlag(Flag::UserInput);
-  enabled = true;
 
   // save player info
   self = bw->self();
@@ -20,11 +19,12 @@ void DevAIModule::onStart()
   // save map info
   mapH = bw->mapHeight();
   mapW = bw->mapWidth();
+
+  bw->setCommandOptimizationLevel(4);
 }
 
 void DevAIModule::onEnd(bool isWinner)
 {
-
 }
 
 DWORD dwLastTickCount;
@@ -33,27 +33,120 @@ void DevAIModule::onFrame()
   if ( bw->isReplay() )
     return;
 
-  if ( !enabled )
+  if ( bw->getFrameCount() % 4 != 0 )
     return;
-
-  for each ( Bullet *b in bw->getBullets() )
+  for each ( Unit *u in self->getUnits() )
   {
-    if ( b && b->exists() )
+    if ( u->getType() != UnitTypes::Hero_Tom_Kazansky || !u->exists() )
+      continue;
+
+    DWORD dwValue = 0;
+    for each ( Unit *e in u->getUnitsInRadius(160) )
     {
-      bw->drawTextMap(b->getPosition().x(), b->getPosition().y(), "%cO", b->getPlayer() ? b->getPlayer()->getTextColor() : 2);
-      bw->drawTextMap(b->getPosition().x(), b->getPosition().y(), "%cX", b->getPlayer() ? b->getPlayer()->getTextColor() : 2);
+      if ( !self->isEnemy(e->getPlayer()) || !e->exists() || !e->isVisible() )
+        continue;
+      int x = e->getPosition().x() - u->getPosition().x();
+      int y = e->getPosition().y() - u->getPosition().y();
+      if ( x < -UnitTypes::Terran_Wraith.dimensionLeft() )
+        dwValue |= 1;
+      if ( x > +UnitTypes::Terran_Wraith.dimensionRight() )
+        dwValue |= 2;
+      if ( y < -UnitTypes::Terran_Wraith.dimensionUp() )
+        dwValue |= 4;
+      if ( y > +UnitTypes::Terran_Wraith.dimensionDown() )
+        dwValue |= 8;
+    }
+    int nx = u->getPosition().x();
+    int ny = u->getPosition().y();
+    if ( nx < 200 )
+      dwValue |= 1;
+    if ( nx > 96*32 - 200 )
+      dwValue |= 2;
+    if ( ny < 250 )
+      dwValue |= 4;
+    if ( ny > 96*32 - 200 )
+      dwValue |= 8;
+
+    bool bottom = ny > 64*32;
+    bool right  = nx > 64*32;
+    switch ( dwValue )
+    {
+    case 1: // from left
+      u->rightClick(Position(nx + 200, ny));
+      break;
+    case 2: // right
+      u->rightClick(Position(nx - 200, ny));
+      break;
+    case 3: // left, right
+      u->rightClick(Position(nx, ny + (bottom ? -200 : 200)));
+      break;
+    case 4: // top
+      u->rightClick(Position(nx, ny + 200));
+      break;
+    case 5: // top left
+      u->rightClick(Position(nx + 140, ny + 140));
+      break;
+    case 6: // top right
+      u->rightClick(Position(nx - 140, ny + 140));
+      break;
+    case 7: // top left right
+      u->rightClick(Position(nx, ny + 200));
+      break;
+    case 8: // bottom
+      u->rightClick(Position(nx, ny - 200));
+      break;
+    case 9: // bottom left
+      u->rightClick(Position(nx + 140, ny - 140));
+      break;
+    case 10: // bottom right
+      u->rightClick(Position(nx - 140, ny - 140));
+      break;
+    case 11: // bottom left right
+      u->rightClick(Position(nx, ny - 200));
+      break;
+    case 12: // bottom top
+      u->rightClick(Position(nx + (right ? -200 : 200), ny));
+      break;
+    case 13: // bottom top left
+      u->rightClick(Position(nx + 200, ny));
+      break;
+    case 14: // bottom top right
+      u->rightClick(Position(nx - 200, ny));
+      break;
+    }
+
+    bool enemynear = false;
+    for each ( Unit *e in u->getUnitsInRadius(160) )
+    {
+      if ( !self->isEnemy(e->getPlayer()) || !e->exists() || !e->isVisible() )
+        continue;
+      enemynear = true;
+      break;
+    }
+    if ( !enemynear )
+    {
+      int shortest = 999999;
+      Unit *closest = NULL;
+      for each ( Unit *e in bw->getAllUnits() )
+      {
+        if ( !e->exists() || !self->isEnemy(e->getPlayer()) || e->isInvincible() || !e->isVisible() )
+          continue;
+        int dist = u->getDistance(e);
+        if ( dist < shortest )
+        {
+          shortest = dist;
+          closest  = e;
+        }
+      }
+      if ( closest && u->getAirWeaponCooldown() == 0 )
+        u->rightClick(closest);
     }
   }
 }
 
 void DevAIModule::onSendText(std::string text)
 {
-  if ( text == "/t" )
-  {
-    enabled = !enabled;
-    Broodwar->printf("DevAITest %s", enabled ? "ENABLED" : "DISABLED");
-  }
-  else if ( text == "/wiki" )
+  if ( text == "/wiki" )
   {
     writeUnitWiki();
     writeWeaponWiki();
@@ -67,6 +160,8 @@ void DevAIModule::onSendText(std::string text)
 
 void DevAIModule::onReceiveText(BWAPI::Player* player, std::string text)
 {
+  if ( text == "?trigger" )
+    Broodwar->sendText("The bot's current trigger is-... Hey wait a minute!");
 }
 
 void DevAIModule::onPlayerLeft(BWAPI::Player* player)
@@ -95,10 +190,19 @@ void DevAIModule::onUnitHide(BWAPI::Unit* unit)
 
 void DevAIModule::onUnitCreate(BWAPI::Unit* unit)
 {
+  if ( unit->getType() == UnitTypes::Hero_Tom_Kazansky && 
+       unit->getPlayer() && 
+       self->isAlly(unit->getPlayer()) &&
+       bw->elapsedTime() > 10 )
+    bw->sendText("Welcome back %s!", unit->getPlayer()->getName().c_str() );
 }
 
 void DevAIModule::onUnitDestroy(BWAPI::Unit* unit)
 {
+  if ( unit->getType() != UnitTypes::Hero_Tom_Kazansky || !unit->getPlayer() || !self->isAlly(unit->getPlayer()) )
+    return;
+
+  bw->sendText("Oh Shi-! We just lost %s!", unit->getPlayer()->getName().c_str());
 }
 
 void DevAIModule::onUnitMorph(BWAPI::Unit* unit)
@@ -111,4 +215,5 @@ void DevAIModule::onUnitRenegade(BWAPI::Unit* unit)
 
 void DevAIModule::onSaveGame(std::string gameName)
 {
+  bw->sendText("Thanks for saving the game as \"%s\"!", gameName.c_str());
 }
