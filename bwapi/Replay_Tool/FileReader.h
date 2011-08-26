3794a3b28515af99fbc8d0cb01a3b3a259c0794c
Adam Heinermann <aheinerm@gmail.com>
2011-08-26 20:39:34 +0000

    Added Replay_Tool project which doesn't work. I don't know what the problem is. I've been debugging it for a while and nothing. If anyone can fix it I would happily continue working on it.
    git-svn-id: https://bwapi.googlecode.com/svn/trunk@3875 273ecf2c-f24b-0410-b6a7-e9298bb4873f

---
+++ bwapi/Replay_Tool/FileReader.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <windows.h>
+#include <string>
+
+class FileReader
+{
+public:
+  FileReader();
+  FileReader(const void *pData, DWORD dwDataSize);
+  ~FileReader();
+  bool Open(const char *pszFilename);
+  bool Error(const char *pszText = NULL);
+  void Free();
+  bool Eof();
+  template <class _T>
+  _T Read()
+  {
+    _T rval;
+    if ( dwOffset + sizeof(_T) <= dwFileSize )
+    {
+      rval = *(_T*)&this->pMem[dwOffset];
+      dwOffset += sizeof(_T);
+    }
+    else
+    {
+      memset(&rval, 0, sizeof(_T));
+      this->eof = true;
+    }
+    return rval;
+  }
+  void Read(void *pBuffer, DWORD dwSize);
+  int         Read7BitEncodedInt();
+  std::string ReadString();
+  DWORD       GetSize();
+  //FileReader  Decompress(DWORD dwCompressedSize, DWORD dwDecompressedSize);  // Requires XNA header
+private:
+  HANDLE  hFile;
+  BYTE    *pMem;
+  DWORD   dwFileSize;
+  DWORD   dwOffset;
+  bool    eof;
+};
+
