b9822ee04986451f59a7e896d812c5f9394a01e6
Adam Heinermann <aheinerm@gmail.com>
2014-12-18 21:55:40 -0500

    Fixed a bug where Player::isObserver would incorrectly return true for some UMS maps.

--- bwapi/BWAPI/Source/BWAPI/GameEvents.cpp
+++ bwapi/BWAPI/Source/BWAPI/GameEvents.cpp
@@ -52,26 +52,29 @@ namespace BWAPI
 
     /* roughly identify which players can possibly participate in this game */
     // iterate triggers for each player
-    for ( int i = 0; i < PLAYABLE_PLAYER_COUNT; ++i )
+    for (int i = 0; i < PLAYABLE_PLAYER_COUNT; ++i)
     {
       // reset participation and resources
-      if ( this->players[i] )
+      if (this->players[i])
       {
         this->players[i]->setParticipating(false);
         this->players[i]->resetResources();
       }
 
       // First check if player owns a unit at start
-      for ( int u = 0; u < BW::UnitID::None; ++u )
+      for (int u = 0; u < UnitTypes::None; ++u)
       {
-        if ( BW::BWDATA_AllScores->unitCounts.all[u][i] )
+        if (BW::BWDATA_AllScores->unitCounts.all[u][i])
         {
-          if ( this->players[i] )
+          if (this->players[i])
             this->players[i]->setParticipating();
           break;
         }
       }
+    }
 
+    for (int i = 0; i < PLAYABLE_PLAYER_COUNT; ++i)
+    {
       // Then iterate each trigger
       // checking if a unit can be created or given to the player later in the game
       for ( BW::BlizzVectorEntry<BW::Triggers::Trigger> *t = BW::BWDATA_TriggerVectors[i].begin; (u32)t != ~(u32)&BW::BWDATA_TriggerVectors[i].end && (u32)t != (u32)&BW::BWDATA_TriggerVectors[i].begin; t = t->next )
