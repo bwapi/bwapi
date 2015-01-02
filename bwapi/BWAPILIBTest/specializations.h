8d50b0dcafa23bf76345840b17d9761a0248e89c
Adam Heinermann <aheinerm@gmail.com>
2015-01-02 17:19:57 -0500

    Added BWAPILibTest project and first unit tests.

---
+++ bwapi/BWAPILIBTest/specializations.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <BWAPI.h>
+
+namespace Microsoft{
+  namespace VisualStudio {
+    namespace CppUnitTestFramework
+    {
+      
+#define SPECIALIZE(N) \
+      template <> static std::wstring ToString(const BWAPI::N& q) { RETURN_WIDE_STRING(q); } \
+      template <> static std::wstring ToString(const BWAPI::N* q) { RETURN_WIDE_STRING(q); } \
+      template <> static std::wstring ToString(BWAPI::N* q)       { RETURN_WIDE_STRING(q); }
+
+      SPECIALIZE(BulletType);
+      SPECIALIZE(Color);
+      SPECIALIZE(DamageType);
+      SPECIALIZE(Error);
+      SPECIALIZE(ExplosionType);
+      SPECIALIZE(GameType);
+      SPECIALIZE(Order);
+      SPECIALIZE(PlayerType);
+      SPECIALIZE(Race);
+      SPECIALIZE(TechType);
+      SPECIALIZE(UnitSizeType);
+      SPECIALIZE(UnitType);
+      SPECIALIZE(UpgradeType);
+      SPECIALIZE(WeaponType);
+
+      SPECIALIZE(Position);
+      SPECIALIZE(TilePosition);
+      SPECIALIZE(WalkPosition);
+
+#undef SPECIALIZE
+    }
+  }
+}
