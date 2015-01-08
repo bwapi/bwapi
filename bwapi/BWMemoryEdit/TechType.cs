a43f9b765d93e948e25fad78ab1f8840132db129
Adam Heinermann <aheinerm@gmail.com>
2015-01-07 21:14:10 -0500

    Auto-update info and add compare for BWMemEdit.

---
+++ bwapi/BWMemoryEdit/TechType.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWMemoryEdit
+{
+    public enum TechType : byte
+    {
+        Stim_Packs = 0,
+        Lockdown,
+        EMP_Shockwave,
+        Spider_Mines,
+        Scanner_Sweep,
+        Tank_Siege_Mode,
+        Defensive_Matrix,
+        Irradiate,
+        Yamato_Gun,
+        Cloaking_Field,
+        Personnel_Cloaking,
+        Burrowing,
+        Infestation,
+        Spawn_Broodlings,
+        Dark_Swarm,
+        Plague,
+        Consume,
+        Ensnare,
+        Parasite,
+        Psionic_Storm,
+        Hallucination,
+        Recall,
+        Stasis_Field,
+        Archon_Warp,
+        Restoration,
+        Disruption_Web,
+        Unused_26,
+        Mind_Control,
+        Dark_Archon_Meld,
+        Feedback,
+        Optical_Flare,
+        Maelstrom,
+        Lurker_Aspect,
+        Unused_33,
+        Healing,
+
+        None = 44,
+        Nuclear_Strike,
+        Unknown,
+        MAX
+    }
+}
