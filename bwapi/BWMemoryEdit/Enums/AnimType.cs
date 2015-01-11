3312f7eb3061ffa862f7995dcdf1c5faf2f6e5df
Adam Heinermann <aheinerm@gmail.com>
2015-01-11 09:13:26 -0500

    BWMemEdit Added Images and various flags.

---
+++ bwapi/BWMemoryEdit/Enums/AnimType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWMemoryEdit.Enums
+{
+    public enum AnimType : byte
+    {
+        Init = 0,
+        Death,
+        GndAttkInit,
+        AirAttkInit,
+        Unused1,
+        GndAttkRpt,
+        AirAttkRpt,
+        CastSpell,
+        GndAttkToIdle,
+        AirAttkToIdle,
+        Unused2,
+        Walking,
+        WalkingToIdle,
+        SpecialState1,
+        SpecialState2,
+        AlmostBuilt,
+        Built,
+        Landing,
+        LiftOff,
+        IsWorking,
+        WorkingToIdle,
+        WarpIn,
+        Unused3,
+        StarEditInit,
+        Disable,
+        Burrow,
+        UnBurrow,
+        Enable        
+    }
+}
---
+++ bwapi/BWMemoryEdit/Enums/RLE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWMemoryEdit.Enums
+{
+    public enum RLE : byte
+    {
+        RLE_NORMAL,
+        RLE_1,
+        RLE_2,
+        RLE_3,
+        RLE_4,
+        RLE_CLOAK,
+        RLE_CLOAKED,
+        RLE_DECLOAK,
+        RLE_8,
+        RLE_EFFECT,
+        RLE_SHADOW,
+        RLE_HPFLOATDRAW,
+        RLE_WARP,
+        RLE_OUTLINE,
+        RLE_PLAYER_SIDE,
+        RLE_15,
+        RLE_SHIFT,
+        RLE_FIRE
+    }
+}
