085c80d6141cefb10beb41119c57ae66b334d0a7
Adam Heinermann <aheinerm@gmail.com>
2015-02-07 01:28:25 -0500

    #546 canMake now correctly rejects Zerglings/Scourges on half-supply deficits.

--- bwapi/DevAIModule/Source/DevAIModule.cpp
+++ bwapi/DevAIModule/Source/DevAIModule.cpp
@@ -53,19 +53,7 @@ void DevAIModule::onFrame()
   if ( bw->self() )
   {
     Unitset myUnits = bw->getSelectedUnits();
-    for ( auto &u : myUnits )
-    {
-      if (u->getType() == UnitTypes::Protoss_Probe)
-      {
-        if (!u->build(UnitTypes::Protoss_Shield_Battery, Broodwar->getBuildLocation(UnitTypes::Protoss_Shield_Battery, u->getTilePosition())))
-          bw->drawTextMap(u->getPosition(), "%s", bw->getLastError().c_str());
-      }
-      else if (u->getType().isCloakable() && !u->cloak())
-      {
-        bw->drawTextMap(u->getPosition(), "%s", bw->getLastError().c_str());
-      }
-      
-    } // for
+    myUnits.morph(UnitTypes::Zerg_Zergling);
   }
 }
 
