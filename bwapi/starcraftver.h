b4657b5d7b43420507f9aa9a1e27c239444596d9
Adam Heinermann <aheinerm@gmail.com>
2015-02-21 02:54:18 -0500

    #544 Project now compiles smoothly without git.
    Changed revision generator script from batch to vbscript.
    Fixed an issue where projects would fail to compile if svnrev was deleted
    after SVNRevGen project was built.

--- bwapi/starcraftver.h
+++ bwapi/starcraftver.h
@@ -6,5 +6,13 @@
 #define SC_VER_3 1
 #define SC_VER_4 1
 
+#ifdef _DEBUG
+  #define BUILD_STR "DEBUG"
+  #define BUILD_DEBUG 1
+#else
+  #define BUILD_STR "RELEASE"
+  #define BUILD_DEBUG 0
+#endif
+
 #define BWAPI_HOME_URL "http://bwapi.github.io/"
 #endif
