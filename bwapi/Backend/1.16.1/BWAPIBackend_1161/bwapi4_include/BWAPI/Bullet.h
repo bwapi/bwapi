321ffc34d62341d752a56f8e2781eabc97bc4d7d
Adam Heinermann <aheinerm@gmail.com>
2019-06-30 18:25:13 -0700

    Cut out backend documentation meant for frontend

--- bwapi/Backend/1.16.1/BWAPIBackend_1161/bwapi4_include/BWAPI/Bullet.h
+++ bwapi/Backend/1.16.1/BWAPIBackend_1161/bwapi4_include/BWAPI/Bullet.h
@@ -36,134 +36,30 @@ namespace BWAPI
   protected:
     virtual ~BulletInterface() {};
   public:
-    /// <summary>Retrieves a unique identifier for the current Bullet.</summary>
-    ///
-    /// @returns
-    ///   An integer value containing the identifier.
     virtual int getID() const = 0;
     
-    /// <summary>Checks if the Bullet exists in the view of the BWAPI player.</summary>
-    ///
-    /// @retval true If the bullet exists or is visible.
-    /// @retval false If the bullet was destroyed or has gone out of scope.
-    ///
-    /// If Flag::CompleteMapInformation is disabled, and a Bullet is not visible, then the
-    /// return value will be false regardless of the Bullet's true existence. This is because
-    /// absolutely no state information on invisible enemy bullets is made available to the AI.
-    ///
-    /// If Flag::CompleteMapInformation is enabled, then this function is accurate for all
-    /// Bullet information.
-    /// @see isVisible, UnitInterface::exists
     virtual bool exists() const = 0;
 
-    /// <summary>Retrieves the Player interface that owns the Bullet.</summary>
-    ///
-    /// @retval nullptr If the Player object for this Bullet is inaccessible.
-    ///
-    /// @returns
-    ///   The owning Player interface object.
     virtual Player getPlayer() const = 0;
 
-    /// <summary>Retrieves the type of this Bullet.</summary>
-    ///
-    /// @retval BulletTypes::Unknown if the Bullet is inaccessible.
-    ///
-    /// @returns
-    ///   A BulletType representing the Bullet's type.
     virtual BulletType getType() const = 0;
 
-    /// <summary>Retrieves the Unit interface that the Bullet spawned from.</summary>
-    ///
-    /// @retval nullptr If the source can not be identified or is inaccessible.
-    ///
-    /// @returns
-    ///   The owning Unit interface object.
-    /// @see getTarget
     virtual Unit getSource() const = 0;
 
-    /// <summary>Retrieves the Bullet's current position.</summary>
-    ///
-    /// @retval Positions::Unknown If the Bullet is inaccessible.
-    ///
-    /// @returns
-    ///   A Position containing the Bullet's current coordinates.
-    /// @see getTargetPosition
     virtual Position getPosition() const = 0;
 
-    /// <summary>Retrieve's the direction the Bullet is facing.</summary> If the angle is 0, then
-    /// the Bullet is facing right.
-    /// 
-    /// @retval 0.0 If the bullet is inaccessible.
-    ///
-    /// @returns
-    ///   A double representing the direction the Bullet is facing.
     virtual double getAngle() const = 0;
 
-    /// <summary>Retrieves the X component of the Bullet's velocity, measured in pixels per frame.</summary>
-    ///
-    /// @retval 0.0 if the Bullet is inaccessible.
-    ///
-    /// @returns
-    ///   A double representing the number of pixels moved on the X axis per frame.
-    ///
-    /// @see getVelocityY, getAngle
     virtual double getVelocityX() const = 0;
 
-    /// <summary>Retrieves the Y component of the Bullet's velocity, measured in pixels per frame.</summary>
-    ///
-    /// @retval 0.0 if the Bullet is inaccessible.
-    ///
-    /// @returns
-    ///   A double representing the number of pixels moved on the Y axis per frame.
-    ///
-    /// @see getVelocityX, getAngle
     virtual double getVelocityY() const = 0;
 
-    /// <summary>Retrieves the Unit interface that the Bullet is heading to.</summary>
-    ///
-    /// @retval nullptr If the Bullet's target Unit is inaccessible, the Bullet is targetting the
-    /// ground, or if the Bullet itself is inaccessible.
-    ///
-    /// @returns
-    ///   The target Unit interface object, if one exists.
-    /// @see getTargetPosition, getSource
     virtual Unit getTarget() const = 0;
 
-    /// <summary>Retrieves the target position that the Bullet is heading to.</summary>
-    ///
-    /// @retval Positions::Unknown If the bullet is inaccessible.
-    ///
-    /// @returns
-    ///   A Position indicating where the Bullet is headed.
-    /// @see getTarget, getPosition
     virtual Position getTargetPosition() const = 0;
 
-    /// <summary>Retrieves the timer that indicates the Bullet's life span.</summary>
-    ///
-    /// Bullets are not permanent objects, so they will often have a limited life span.
-    /// This life span is measured in frames. Normally a Bullet will reach its target
-    /// before being removed.
-    ///
-    /// @retval 0 If the Bullet is inaccessible.
-    ///
-    /// @returns
-    ///   An integer representing the remaining number of frames until the Bullet self-destructs.
     virtual int getRemoveTimer() const = 0;
 
-    /// <summary>Retrieves the visibility state of the Bullet.</summary>
-    ///
-    /// <param name="player"> (optional)
-    ///   If this parameter is specified, then the Bullet's visibility to the given player is
-    ///   checked. If this parameter is omitted, then a default value of nullptr is used, which
-    ///   will check if the BWAPI player has vision of the Bullet.
-    /// </param>
-    ///
-    /// @note If \c player is nullptr and Broodwar->self() is also nullptr, then the visibility of
-    /// the Bullet is determined by checking if at least one other player has vision of the
-    /// Bullet.
-    ///
-    /// @retval true If the Bullet is visible to the specified player.
-    /// @retval false If the Bullet is not visible to the specified player.
     virtual bool isVisible(Player player = nullptr) const = 0;
   };
 }
--- bwapi/Backend/1.16.1/BWAPIBackend_1161/bwapi4_include/BWAPI/Force.h
+++ bwapi/Backend/1.16.1/BWAPIBackend_1161/bwapi4_include/BWAPI/Force.h
@@ -21,44 +21,10 @@ namespace BWAPI
   protected:
     virtual ~ForceInterface() {};
   public :
-    /// <summary>Retrieves the unique ID that represents this Force.</summary>
-    ///
-    /// @returns
-    ///   An integer containing the ID for the Force.
     virtual int getID() const = 0;
 
-    /// <summary>Retrieves the name of the Force.</summary>
-    ///
-    /// @returns
-    ///   A std::string object containing the name of the force.
-    ///
-    /// Example usage:
-    /// @code
-    ///   BWAPI::Force myForce = BWAPI::Broodwar->self()->getForce();
-    ///   if ( myForce->getName() == "Observers" )
-    ///     BWAPI::Broodwar << "Looks like we're observing a match." << std::endl;
-    /// @endcode
-    ///
-    /// @note Don't forget to use std::string::c_str() when passing this parameter to
-    /// Game::sendText and other variadic functions.
     virtual std::string getName() const = 0;
 
-    /// <summary>Retrieves the set of players that belong to this Force.</summary>
-    ///
-    /// @returns
-    ///   A Playerset object containing the players that are part of this Force.
-    ///
-    /// Example usage:
-    /// @code
-    ///   // Get the enemy force, but make sure we have an enemy
-    ///   BWAPI::Force myEnemyForce = BWAPI::Broodwar->enemy() ? BWAPI::Broodwar->enemy()->getForce() : nullptr;
-    ///   if ( myEnemyForce != nullptr )
-    ///   {
-    ///     Broodwar << "The allies of my enemy are..." << std::endl;
-    ///     for ( auto i = myEnemyForce.begin(); i != myEnemyForce.end(); ++i )
-    ///       Broodwar << "  - " << i->getName() << std::endl;
-    ///   }
-    /// @endcode
     virtual Playerset getPlayers() const = 0;
   };
 }
